1eba2a1c67d54bfb8ea6eff24e1bdb008610b8a2
guykatzz <katz911@gmail.com>
2017-03-10 10:55:41 -0800

    errno

---
+++ src/common/real/Errno.cpp
@@ -0,0 +1,17 @@
+#include <errno.h>
+
+namespace T
+{
+    int errorNumber()
+	{
+		return errno;
+    }
+}
+
+//
+// Local Variables:
+// compile-command: "make -C ../.. "
+// tags-file-name: "../../TAGS"
+// c-basic-offset: 4
+// End:
+//
