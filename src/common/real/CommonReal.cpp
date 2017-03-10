fa865fd0f7bc85051530dc325045690a291d81d2
guykatzz <katz911@gmail.com>
2017-03-10 10:54:59 -0800

    common real

---
+++ src/common/real/CommonReal.cpp
@@ -0,0 +1,12 @@
+#define CXXTEST_MOCK_REAL_SOURCE_FILE
+#include "T/stdlib.h"
+#include "T/sys/stat.h"
+#include "T/unistd.h"
+
+//
+// Local Variables:
+// compile-command: "make -C ../.. "
+// tags-file-name: "../../TAGS"
+// c-basic-offset: 4
+// End:
+//
