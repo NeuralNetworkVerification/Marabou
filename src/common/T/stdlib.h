24624010707678e3d10571266604907c43983ac7
guykatzz <katz911@gmail.com>
2017-03-09 19:34:47 -0800

    stdlib

---
+++ src/common/T/stdlib.h
@@ -0,0 +1,38 @@
+#ifndef __T__Stdlib_h__
+#define __T__Stdlib_h__
+
+#include <cxxtest/Mock.h>
+#include <stdlib.h>
+
+CXXTEST_MOCK_GLOBAL( void *,
+					 malloc,
+					 ( size_t size ),
+					 ( size ) );
+
+CXXTEST_MOCK_VOID_GLOBAL( free,
+						  ( void *ptr ),
+						  ( ptr ) );
+
+CXXTEST_MOCK_GLOBAL( void *,
+                     realloc,
+                     ( void *ptr, size_t size ),
+                     ( ptr, size ) );
+
+CXXTEST_MOCK_VOID_GLOBAL( srand,
+                          ( unsigned seed ),
+                          ( seed ) );
+
+CXXTEST_MOCK_GLOBAL( int,
+                     rand,
+                     (),
+                     () );
+
+#endif // __T__Stdlib_h__
+
+//
+// Local Variables:
+// compile-command: "make -C ../.. "
+// tags-file-name: "../../TAGS"
+// c-basic-offset: 4
+// End:
+//
