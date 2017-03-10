36de9245391790b562066295e562af3b436c86cd
guykatzz <katz911@gmail.com>
2017-03-10 10:15:12 -0800

    pair

---
+++ src/common/Pair.h
@@ -0,0 +1,83 @@
+/*********************                                                        */
+/*! \file Pair.h
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Guy Katz
+ ** This file is part of the Reluplex project.
+ ** Copyright (c) 2016-2017 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved. See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **/
+
+#ifndef __Pair_h__
+#define __Pair_h__
+
+#include <iostream>
+#include <utility>
+
+template<class L, class R>
+class Pair
+{
+    typedef std::pair<L,R> Super;
+public:
+    Pair()
+    {
+    }
+
+    Pair( const L &first, const R &second ) : _container( first, second )
+    {
+    }
+
+    L &first()
+    {
+        return _container.first;
+    }
+
+    const L &first() const
+    {
+        return _container.first;
+    }
+
+    R &second()
+    {
+        return _container.second;
+    }
+
+    const R &second() const
+    {
+        return _container.second;
+    }
+
+	Pair<L, R> &operator=( const Pair<L, R> &other )
+	{
+		_container = other._container;
+		return *this;
+	}
+
+    bool operator==( const Pair<L, R> &other ) const
+	{
+		return _container == other._container;
+	}
+
+    bool operator!=( const Pair<L, R> &other ) const
+	{
+		return _container != other._container;
+	}
+
+    bool operator<( const Pair<L, R> &other ) const
+    {
+        return _container < other._container;
+    }
+
+protected:
+    Super _container;
+};
+
+template<class L, class R>
+std::ostream &operator<<( std::ostream &stream, const Pair<L, R> &pair )
+{
+    return stream << pair.first() << "," << pair.second();
+}
+
+#endif // __Pair_h__
---
+++ src/common/tests/Test_Pair.h
@@ -0,0 +1,46 @@
+#include <cxxtest/TestSuite.h>
+
+#include "String.h"
+#include "Pair.h"
+
+class PairTestSuite : public CxxTest::TestSuite
+{
+public:
+
+    void test_pair()
+    {
+        Pair<int, String> p1;
+        Pair<int, String> p2( 5, "dog" );
+
+        TS_ASSERT( p1 != p2 );
+        TS_ASSERT( !( p1 == p2 ) );
+
+        p1.first() = 5;
+
+        TS_ASSERT( p1 != p2 );
+        TS_ASSERT( !( p1 == p2 ) );
+
+        p1.second() = "dog";
+
+        TS_ASSERT( p1 == p2 );
+        TS_ASSERT( !( p1 != p2 ) );
+
+        p2.second() = "bird";
+
+        TS_ASSERT( p1 != p2 );
+        TS_ASSERT( !( p1 == p2 ) );
+
+        p1 = p2;
+
+        TS_ASSERT( p1 == p2 );
+        TS_ASSERT( !( p1 != p2 ) );
+    }
+};
+
+//
+// Local Variables:
+// compile-command: "make -C ../.. "
+// tags-file-name: "../../TAGS"
+// c-basic-offset: 4
+// End:
+//
