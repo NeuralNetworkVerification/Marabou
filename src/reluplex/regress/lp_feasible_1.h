268bb6a47f82f236136493e3d56ad7d4237f10c4
guykatzz <katz911@gmail.com>
2017-06-12 11:33:46 -0700

    a folder for regression tests

---
+++ src/reluplex/regress/lp_feasible_1.h
@@ -0,0 +1,37 @@
+/*********************                                                        */
+/*! \file Lp_Feasible_1.h
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Guy Katz
+ ** This file is part of the Reluplex project.
+ ** Copyright (c) 2016-2017 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved. See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **/
+
+#ifndef __Lp_Feasible_1_h__
+#define __Lp_Feasible_1_h__
+
+#include "Engine.h"
+
+class Lp_Feasible_1
+{
+public:
+    void run()
+    {
+        Engine engine;
+
+        printf( "Hi!\n" );
+    }
+};
+
+#endif // __Lp_Feasible_1_h__
+
+//
+// Local Variables:
+// compile-command: "make -C ../.. "
+// tags-file-name: "../../TAGS"
+// c-basic-offset: 4
+// End:
+//
---
+++ src/reluplex/regress/main_regress.cpp
@@ -0,0 +1,29 @@
+/*********************                                                        */
+/*! \file main_regress.cpp
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Guy Katz
+ ** This file is part of the Reluplex project.
+ ** Copyright (c) 2016-2017 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved. See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **/
+
+#include <cstdio>
+
+#include "lp_feasible_1.h"
+
+int main()
+{
+    Lp_Feasible_1 lpf1;
+    lpf1.run();
+}
+
+//
+// Local Variables:
+// compile-command: "make -C ../.. "
+// tags-file-name: "../../TAGS"
+// c-basic-offset: 4
+// End:
+//
