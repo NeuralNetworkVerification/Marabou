747a46e3349e5cb0e74337ca31056fe7da2e49cd
guykatzz <katz911@gmail.com>
2017-06-21 10:36:24 -0700

    reluwise/reluplex --> marabou

--- src/common/File.cpp
+++ src/common/File.cpp
@@ -3,7 +3,7 @@
 ** \verbatim
 ** Top contributors (to current version):
 **   Guy Katz
-** This file is part of the Reluplex project.
+** This file is part of the Marabou project.
 ** Copyright (c) 2016-2017 by the authors listed in the file AUTHORS
 ** in the top-level source directory) and their institutional affiliations.
 ** All rights reserved. See the file COPYING in the top-level source
