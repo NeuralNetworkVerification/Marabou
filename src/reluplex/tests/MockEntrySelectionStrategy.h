5223d14afaf4fdc7645af5fa55fbee7a0850b98e
guykatzz <katz911@gmail.com>
2017-06-27 10:41:06 -0700

    tests

---
+++ src/reluplex/tests/MockEntrySelectionStrategy.h
@@ -0,0 +1,61 @@
+/*********************                                                        */
+/*! \file MockEntrySelectionStrategy.h
+** \verbatim
+** Top contributors (to current version):
+**   Guy Katz
+** This file is part of the Marabou project.
+** Copyright (c) 2016-2017 by the authors listed in the file AUTHORS
+** in the top-level source directory) and their institutional affiliations.
+** All rights reserved. See the file COPYING in the top-level source
+** directory for licensing information.\endverbatim
+**/
+
+#ifndef __MockEntrySelectionStrategy_h__
+#define __MockEntrySelectionStrategy_h__
+
+#include "EntrySelectionStrategy.h"
+
+class MockEntrySelectionStrategy : public EntrySelectionStrategy
+{
+public:
+	MockEntrySelectionStrategy()
+	{
+		wasCreated = false;
+		wasDiscarded = false;
+	}
+
+	bool wasCreated;
+	bool wasDiscarded;
+
+	void mockConstructor()
+	{
+		TS_ASSERT( !wasCreated );
+		wasCreated = true;
+	}
+
+	void mockDestructor()
+	{
+		TS_ASSERT( wasCreated );
+		TS_ASSERT( !wasDiscarded );
+		wasDiscarded = true;
+	}
+
+    List<unsigned> lastCandidates;
+    unsigned nextSelectResult;
+
+    unsigned select( const List<unsigned> &candidates, const ITableau &/* tableau */ )
+    {
+        lastCandidates = candidates;
+        return nextSelectResult;
+    }
+};
+
+#endif // __MockEntrySelectionStrategy_h__
+
+//
+// Local Variables:
+// compile-command: "make -C ../.. "
+// tags-file-name: "../../TAGS"
+// c-basic-offset: 4
+// End:
+//
---
+++ src/reluplex/tests/Test_DantzigsRule.h
@@ -0,0 +1,87 @@
+/*********************                                                        */
+/*! \file Test_DantzigsRule.h
+** \verbatim
+** Top contributors (to current version):
+**   Guy Katz
+** This file is part of the Marabou project.
+** Copyright (c) 2016-2017 by the authors listed in the file AUTHORS
+** in the top-level source directory) and their institutional affiliations.
+** All rights reserved. See the file COPYING in the top-level source
+** directory for licensing information.\endverbatim
+**/
+
+#include <cxxtest/TestSuite.h>
+
+#include "DantzigsRule.h"
+#include "MockTableau.h"
+#include "ReluplexError.h"
+
+#include <string.h>
+
+class MockForDantzigsRule
+{
+public:
+};
+
+class DantzigsRuleTestSuite : public CxxTest::TestSuite
+{
+public:
+    MockForDantzigsRule *mock;
+    MockTableau *tableau;
+
+    void setUp()
+    {
+        TS_ASSERT( mock = new MockForDantzigsRule );
+        TS_ASSERT( tableau = new MockTableau );
+
+    }
+
+    void tearDown()
+    {
+        TS_ASSERT_THROWS_NOTHING( delete tableau );
+        TS_ASSERT_THROWS_NOTHING( delete mock );
+    }
+
+    void test_select()
+    {
+        DantzigsRule dantzigsRule;
+
+        List<unsigned> candidates;
+
+        TS_ASSERT_THROWS_EQUALS( dantzigsRule.select( candidates, *tableau ),
+                                 const ReluplexError &e,
+                                 e.getCode(),
+                                 ReluplexError::NO_AVAILABLE_CANDIDATES );
+
+        tableau->setDimensions( 10, 100 );
+
+        candidates.append( 2 );
+        candidates.append( 3 );
+        candidates.append( 10 );
+        candidates.append( 51 );
+
+        tableau->nextCostFunction[2] = -5;
+        tableau->nextCostFunction[3] = 7;
+        tableau->nextCostFunction[10] = -15;
+        tableau->nextCostFunction[51] = 12;
+
+        // These guys are not candidates, so their coefficients don't matter
+        tableau->nextCostFunction[14] = 102;
+        tableau->nextCostFunction[25] = -1202;
+        tableau->nextCostFunction[33] = 10;
+
+        TS_ASSERT_EQUALS( dantzigsRule.select( candidates, *tableau ), 10U );
+
+        candidates.append( 25 );
+
+        TS_ASSERT_EQUALS( dantzigsRule.select( candidates, *tableau ), 25U );
+    }
+};
+
+//
+// Local Variables:
+// compile-command: "make -C ../.. "
+// tags-file-name: "../../TAGS"
+// c-basic-offset: 4
+// End:
+//
