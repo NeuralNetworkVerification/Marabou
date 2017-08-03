17ebb8404dba458f9781e1f6a8e343024e5be375
guykatzz <katz911@gmail.com>
2017-08-03 09:08:41 -0700

    forgot to include the new files

---
+++ src/reluplex/EngineState.h
@@ -0,0 +1,35 @@
+/*********************                                                        */
+/*! \file EngineState.h
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Guy Katz
+ ** This file is part of the Marabou project.
+ ** Copyright (c) 2016-2017 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved. See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **/
+
+#ifndef __EngineState_h__
+#define __EngineState_h__
+
+#include "TableauState.h"
+
+class EngineState
+{
+public:
+    EngineState();
+    ~EngineState();
+
+    TableauState _tableauState;
+};
+
+#endif // __EngineState_h__
+
+//
+// Local Variables:
+// compile-command: "make -C .. "
+// tags-file-name: "../TAGS"
+// c-basic-offset: 4
+// End:
+//
