6d40fcc4b139efeb740c3e85f66c94548be7d777
guykatzz <katz911@gmail.com>
2017-08-17 14:55:17 -0700

    oops

---
+++ src/reluplex/EntrySelectionStrategy.cpp
@@ -0,0 +1,33 @@
+/*********************                                                        */
+/*! \file EntrySelectionStrategy.cpp
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Guy Katz
+ ** This file is part of the Marabou project.
+ ** Copyright (c) 2016-2017 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved. See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **/
+
+#include "EntrySelectionStrategy.h"
+
+#include <cstring>
+
+EntrySelectionStrategy::EntrySelectionStrategy()
+    : _statistics( NULL )
+{
+}
+
+void EntrySelectionStrategy::setStatistics( Statistics *statistics )
+{
+    _statistics = statistics;
+}
+
+//
+// Local Variables:
+// compile-command: "make -C .. "
+// tags-file-name: "../TAGS"
+// c-basic-offset: 4
+// End:
+//
