59a8f7ae3bf754b2ef8b68d7ba0f4627cf39f277
Christopher Lazarus <clazarusg@gmail.com>
2018-03-16 16:13:30 -0700

    maraboupy (#9)
    Introducing maraboupy: a python module that wraps Marabou and enables easy interaction with Tensorflow networks and .nnet files.
    
    * Python Wrapper
    
    -Abstract classes needed for maraboupy
    -TF Parser
    -NNet Parser
    -Example scripts
    -Example Notebooks
    -Example TF network proto buffers.
    
    
    * Refactored addReluConstraint into MarabouCore
    
    * Left all original Marabou code untouched and moved all needed modifications to maraboucore.cpp in maraboupy.
    
    * Maraboupy now uses the same Rules.mk as Marabou and uses its own Makefile for its flags.

---
+++ maraboupy/examples/MarabouNetworkTFExample.py
@@ -0,0 +1,53 @@
+from maraboupy import Marabou
+import numpy as np
+
+
+# Network corresponds to inputs x0, x1
+# Outputs: y0 = |x0| + |x1|, y1 = x0^2 + x1^2
+filename = './networks/graph_test_medium.pb'
+network = Marabou.read_tf(filename)
+
+## Or, you can specify the operation names of the input and output operations
+## By default chooses the only placeholder as input, last op as output
+#inputName = 'Placeholder'
+#outputName = 'y_out'
+#network = MarabouNetworkTF(filename=filename, inputName=inputName, outputName = outputName)
+
+# Get the input and output variable numbers; [0] since first dimension is batch size
+inputVars = network.inputVars[0]
+outputVars = network.outputVars[0]
+
+# Set input bounds
+network.setLowerBound(inputVars[0],-10.0)
+network.setUpperBound(inputVars[0], -10.0)
+network.setLowerBound(inputVars[1],-10.0)
+network.setUpperBound(inputVars[1], -10.0)
+
+# Set output bounds
+network.setLowerBound(outputVars[1], 194.0)
+network.setUpperBound(outputVars[1], 210.0)
+
+# Call to C++ Marabou solver
+vals = network.solve("marabou.log")
+
+# Print results
+if len(vals)==0:
+    print("UNSAT")
+else:
+    print("SAT")
+    print("Input Values:")
+    for i in range(inputVars.size):
+        print("%d: %.4e" % (i, vals[inputVars[i]]))
+              
+    print("\nOutput Values:")
+    for i in range(outputVars.size):
+        print("%d: %.4e" % (i, vals[outputVars[i]]))
+
+    inputValues = [vals[x] for x in inputVars]
+    outputValues = [vals[x] for x in outputVars]
+    error = network.findError(inputValues)
+    print("\nError norm:")
+    print(np.linalg.norm(error))
+
+
+
