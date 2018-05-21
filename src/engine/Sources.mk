00f4fec80ef3a12fb89eabf3f6257b61ba5aeb74
Guy Katz <guykatz@cs.huji.ac.il>
2018-05-21 13:28:46 +0300

    adding a single source file now only entails changing one makefile

---
+++ src/engine/Sources.mk
@@ -0,0 +1,33 @@
+SOURCES += \
+	BlandsRule.cpp \
+	ConstraintMatrixAnalyzer.cpp \
+	CostFunctionManager.cpp \
+	DantzigsRule.cpp \
+	DegradationChecker.cpp \
+	Engine.cpp \
+	EngineState.cpp \
+	EntrySelectionStrategy.cpp \
+	Equation.cpp \
+	FreshVariables.cpp \
+	InputQuery.cpp \
+	MaxConstraint.cpp \
+	PiecewiseLinearCaseSplit.cpp \
+	PiecewiseLinearConstraint.cpp \
+	PrecisionRestorer.cpp \
+	Preprocessor.cpp \
+	ProjectedSteepestEdge.cpp \
+	ReluConstraint.cpp \
+	RowBoundTightener.cpp \
+	SmtCore.cpp \
+	Statistics.cpp \
+	Tableau.cpp \
+	TableauRow.cpp \
+	TableauState.cpp \
+
+#
+# Local Variables:
+# compile-command: "make -C ../.. "
+# tags-file-name: "../../TAGS"
+# c-basic-offset: 4
+# End:
+#
