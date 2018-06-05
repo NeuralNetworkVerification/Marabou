b35e11e1f75c4fb7e1b0c401c8f5170548f8f136
Guy Katz <guykatz@cs.huji.ac.il>
2018-06-05 13:11:32 +0300

    more tests

--- src/basis_factorization/tests/Test_GaussianEliminator.h
+++ src/basis_factorization/tests/Test_GaussianEliminator.h
@@ -13,6 +13,7 @@
 
 #include <cxxtest/TestSuite.h>
 
+#include "BasisFactorizationError.h"
 #include "EtaMatrix.h"
 #include "FloatUtils.h"
 #include "GaussianEliminator.h"
@@ -151,6 +152,52 @@ public:
             }
 
             TS_ASSERT_THROWS_NOTHING( delete lu );
+            TS_ASSERT_THROWS_NOTHING( delete ge );
+        }
+
+        {
+            double A[] =
+            {
+                2, 3, -4, 0,
+                -5, 1, 2, 2,
+                0, 4, 1, -5,
+                1, 2, 3, 4,
+            };
+
+            GaussianEliminator *ge;
+            LUFactors *lu;
+
+            TS_ASSERT( ge = new GaussianEliminator( A, 4 ) );
+            TS_ASSERT_THROWS_NOTHING( lu = ge->run() );
+
+            double result[16];
+            computeMatrixFromFactorization( lu, result );
+
+            for ( unsigned i = 0; i < 16; ++i )
+            {
+                TS_ASSERT( FloatUtils::areEqual( A[i], result[i] ) );
+            }
+
+            TS_ASSERT_THROWS_NOTHING( delete lu );
+            TS_ASSERT_THROWS_NOTHING( delete ge );
+        }
+
+        {
+            double A[] =
+            {
+                2, 3, 0,
+                0, 1, 0,
+                5, 4, 0
+            };
+
+            GaussianEliminator *ge;
+
+            TS_ASSERT( ge = new GaussianEliminator( A, 3 ) );
+            TS_ASSERT_THROWS_EQUALS( ge->run(),
+                                     const BasisFactorizationError &e,
+                                     e.getCode(),
+                                     BasisFactorizationError::GAUSSIAN_ELIMINATION_FAILED );
+
             TS_ASSERT_THROWS_NOTHING( delete ge );
         }
     }
