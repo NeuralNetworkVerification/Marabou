05e06deae0cea994baeb7f9625bc37657f8fb7b5
Guy Katz <guykatz@cs.huji.ac.il>
2018-07-12 14:51:41 +0300

    comments and unit tests

--- src/basis_factorization/SparseVector.h
+++ src/basis_factorization/SparseVector.h
@@ -15,33 +15,67 @@
 
 #include "CSRMatrix.h"
 
+/*
+  This class provides supprot for sparse vectors in
+  Compressed Sparse Row (CSR) format. The vector is
+  simply stored as a 1 x n matrix in CSR format.
+*/
+
 class SparseVector
 {
 public:
+    /*
+      Initialization: the size determines the dimension of the
+      underlying 1 x n matrix, and also influences the number
+      of memory allocated initially.
+
+      A vector can be initialized from a dense vector, or it
+      can remain empty.
+    */
     SparseVector();
     SparseVector( unsigned size );
     SparseVector( const double *V, unsigned size );
-
     void initialize( const double *V, unsigned size );
-
     void initializeToEmpty( unsigned size );
 
+    /*
+      Remove the vector's elements, without touching the
+      allocated memory
+    */
     void clear();
 
+    /*
+      The number of non-zero elements in the vector
+    */
     unsigned getNnz() const;
-
     bool empty() const;
 
+    /*
+      Retrieve an element
+    */
     double get( unsigned index );
 
-    void dump() const;
-
+    /*
+      Convert the vector to dense format
+    */
     void toDense( double *result ) const;
 
+    /*
+      Cloning
+    */
     SparseVector &operator=( const SparseVector &other );
 
+    /*
+      For some delicate changes, we can get access to the
+      internal CSR matrix
+    */
     CSRMatrix *getInternalMatrix();
 
+    /*
+      Retrieve an entry by its index in the vector.
+      This is not like "get" - the entry index is a number
+      between 0 and _nnz.
+    */
     unsigned getIndexOfEntry( unsigned entry ) const;
     double getValueOfEntry( unsigned entry ) const;
 
@@ -56,6 +90,11 @@ public:
     */
     void commitChange( unsigned index, double newValue );
     void executeChanges();
+
+    /*
+      For debugging
+    */
+    void dump() const;
 
 private:
     CSRMatrix _V;
