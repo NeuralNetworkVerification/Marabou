5171785416552c6f763c2442ed7a37970b4a61d7
Duligur Ibeling <duligur@gmail.com>
2018-08-14 04:44:22 -0700

    quick fix in SparseGaussianEliminator. add Sources.mk for input parsers (except MpsParser, which is broken). (#85)

---
+++ src/input_parsers/Sources.mk
@@ -0,0 +1,14 @@
+SOURCES += \
+	AcasNeuralNetwork.cpp \
+	AcasNnet.cpp \
+	AcasParser.cpp \
+	BerkeleyNeuralNetwork.cpp \
+	BerkeleyParser.cpp \
+
+#
+# Local Variables:
+# compile-command: "make -C ../.. "
+# tags-file-name: "../../TAGS"
+# c-basic-offset: 4
+# End:
+#
