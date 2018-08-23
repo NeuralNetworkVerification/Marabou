2c5128fd8c651b72a47b5c75098a1bc2718428b1
Guy Katz <guykatz@cs.huji.ac.il>
2018-08-23 09:54:58 +0300

    oops

---
+++ src/basis_factorization/SparseColumnsOfBasis.cpp
@@ -0,0 +1,39 @@
+/*********************                                                        */
+/*! \file SparseColumnsOfBasis.cpp
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Guy Katz
+ ** This file is part of the Marabou project.
+ ** Copyright (c) 2016-2017 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved. See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **/
+
+#include "BasisFactorizationError.h"
+#include "SparseColumnsOfBasis.h"
+
+SparseColumnsOfBasis::SparseColumnsOfBasis( unsigned m )
+    : _columns( NULL )
+{
+    _columns = new const SparseVector *[m];
+    if ( !_columns )
+        throw BasisFactorizationError( BasisFactorizationError::ALLOCATION_FAILED, "SparseColumnsOfBasis::columns" );
+}
+
+SparseColumnsOfBasis::~SparseColumnsOfBasis()
+{
+    if ( _columns )
+    {
+        delete[] _columns;
+        _columns = NULL;
+    }
+}
+
+//
+// Local Variables:
+// compile-command: "make -C ../.. "
+// tags-file-name: "../../TAGS"
+// c-basic-offset: 4
+// End:
+//
---
+++ src/basis_factorization/SparseColumnsOfBasis.h
@@ -0,0 +1,35 @@
+/*********************                                                        */
+/*! \file SparseColumnsOfBasis.h
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Guy Katz
+ ** This file is part of the Marabou project.
+ ** Copyright (c) 2016-2017 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved. See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **/
+
+#ifndef __SparseColumnsOfBasis_h__
+#define __SparseColumnsOfBasis_h__
+
+#include "SparseVector.h"
+
+class SparseColumnsOfBasis
+{
+public:
+    SparseColumnsOfBasis( unsigned m );
+    ~SparseColumnsOfBasis();
+
+    const SparseVector **_columns;
+};
+
+#endif // __SparseColumnsOfBasis_h__
+
+//
+// Local Variables:
+// compile-command: "make -C ../.. "
+// tags-file-name: "../../TAGS"
+// c-basic-offset: 4
+// End:
+//
