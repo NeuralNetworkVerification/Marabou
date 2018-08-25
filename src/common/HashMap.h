5b5c3393691b6152191a6da27fa5290153a984ee
guykatzz <katz911@gmail.com>
2018-08-25 16:37:16 +0300

    hashmaps

---
+++ src/common/HashMap.h
@@ -0,0 +1,127 @@
+/*********************                                                        */
+/*! \file HashMap.h
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Guy Katz
+ ** This file is part of the Marabou project.
+ ** Copyright (c) 2016-2017 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved. See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **/
+
+#ifndef __HashMap_h__
+#define __HashMap_h__
+
+#include "CommonError.h"
+#include "List.h"
+#include "Set.h"
+#include <unordered_map>
+
+template<class Key, class Value>
+class HashMap
+{
+    typedef std::unordered_map<Key,Value> Super;
+public:
+    typedef typename Super::iterator iterator;
+    typedef typename Super::const_iterator const_iterator;
+
+    Value &operator[]( const Key &key )
+    {
+        return _container[key];
+    }
+
+    bool empty() const
+    {
+        return _container.empty();
+    }
+
+    bool exists( const Key &key ) const
+    {
+        return _container.find( key ) != _container.end();
+    }
+
+    unsigned size() const
+    {
+        return _container.size();
+    }
+
+    void erase( Key key )
+    {
+        if ( !exists( key ) )
+            throw CommonError( CommonError::KEY_DOESNT_EXIST_IN_HASHMAP );
+
+        _container.erase( key );
+    }
+
+    iterator begin()
+    {
+        return _container.begin();
+    }
+
+    iterator end()
+    {
+        return _container.end();
+    }
+
+    void clear()
+    {
+        _container.clear();
+    }
+
+    Value get( const Key &key ) const
+    {
+        if ( !exists( key ) )
+            throw CommonError( CommonError::KEY_DOESNT_EXIST_IN_HASHMAP );
+
+        return _container.at( key );
+    }
+
+    bool operator==( const HashMap<Key,Value> &other ) const
+    {
+        return _container == other._container;
+    }
+
+    bool operator!=( const HashMap<Key,Value> &other ) const
+    {
+        return _container != other._container;
+    }
+
+    const Value &at( const Key &key ) const
+    {
+        if ( !exists( key ) )
+            throw CommonError( CommonError::KEY_DOESNT_EXIST_IN_HASHMAP );
+
+        return _container.at( key );
+    }
+
+    Value &at( const Key &key )
+    {
+        if ( !exists( key ) )
+            throw CommonError( CommonError::KEY_DOESNT_EXIST_IN_HASHMAP );
+
+        return _container.at( key );
+    }
+
+    // Set<Key> keys() const
+    // {
+    //     Set<Key> result;
+    //     for ( auto it : _container )
+    //         result.insert( it.first );
+
+    //     return result;
+    // }
+
+private:
+    Super _container;
+};
+
+#endif // __HashMap_h__
+
+//
+// Local Variables:
+// compile-command: "make -C ../.. "
+// tags-file-name: "../../TAGS"
+// c-basic-offset: 4
+// End:
+//
