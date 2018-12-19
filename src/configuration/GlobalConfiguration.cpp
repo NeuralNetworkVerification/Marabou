76ab89dc36d018996f9c02e8151b6b5e4afff9f1
Guy Katz <guykatz@cs.huji.ac.il>
2018-12-19 08:58:18 +0200

    revert threshold, set smart-fix to false by default

--- src/configuration/GlobalConfiguration.cpp
+++ src/configuration/GlobalConfiguration.cpp
@@ -34,7 +34,7 @@ const double GlobalConfiguration::ACCEPTABLE_SIMPLEX_PIVOT_THRESHOLD = 0.0001;
 const bool GlobalConfiguration::USE_COLUMN_MERGING_EQUATIONS = false;
 const double GlobalConfiguration::GAUSSIAN_ELIMINATION_PIVOT_SCALE_THRESHOLD = 0.1;
 const unsigned GlobalConfiguration::MAX_SIMPLEX_PIVOT_SEARCH_ITERATIONS = 5;
-const unsigned GlobalConfiguration::CONSTRAINT_VIOLATION_THRESHOLD = 5;
+const unsigned GlobalConfiguration::CONSTRAINT_VIOLATION_THRESHOLD = 20;
 const unsigned GlobalConfiguration::BOUND_TIGHTING_ON_CONSTRAINT_MATRIX_FREQUENCY = 100;
 const double GlobalConfiguration::COST_FUNCTION_ERROR_THRESHOLD = 0.0000000001;
 
@@ -70,7 +70,7 @@ const bool GlobalConfiguration::PROJECTED_STEEPEST_EDGE_LOGGING = false;
 const bool GlobalConfiguration::GAUSSIAN_ELIMINATION_LOGGING = false;
 const bool GlobalConfiguration::QUERY_LOADER_LOGGING = true;
 
-const bool GlobalConfiguration::USE_SMART_FIX = true;
+const bool GlobalConfiguration::USE_SMART_FIX = false;
 const bool GlobalConfiguration::USE_LEAST_FIX = false;
 
 void GlobalConfiguration::print()
