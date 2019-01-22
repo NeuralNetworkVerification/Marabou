31d8372e72abba383e96ac04aa1b2946f2b7e555
guykatzz <katz911@gmail.com>
2019-01-22 13:39:06 +0200

    cleanup a few cases where the script failed

--- src/basis_factorization/BasisFactorizationError.h
+++ src/basis_factorization/BasisFactorizationError.h
@@ -14,19 +14,6 @@
  ** [[ Add lengthier description here ]]
  **/
 
-
-/*********************                                                        */
-/*! \file BasisFactorizationError.h
- ** \verbatim
- ** Top contributors (to current version):
- **   Guy Katz
- ** This file is part of the Marabou project.
- ** Copyright (c) 2016-2017 by the authors listed in the file AUTHORS
- ** in the top-level source directory) and their institutional affiliations.
- ** All rights reserved. See the file COPYING in the top-level source
- ** directory for licensing information.\endverbatim
- **/
-
 #ifndef __BasisFactorizationError_h__
 #define __BasisFactorizationError_h__
 
--- src/engine/Engine.cpp
+++ src/engine/Engine.cpp
@@ -14,17 +14,6 @@
  ** [[ Add lengthier description here ]]
  **/
 
-/*! \file Engine.cpp
- ** \verbatim
- ** Top contributors (to current version):
- **   Guy Katz
- ** This file is part of the Marabou project.
- ** Copyright (c) 2016-2017 by the authors listed in the file AUTHORS
- ** in the top-level source directory) and their institutional affiliations.
- ** All rights reserved. See the file COPYING in the top-level source
- ** directory for licensing information.\endverbatim
- **/
-
 #include "AutoConstraintMatrixAnalyzer.h"
 #include "Debug.h"
 #include "Engine.h"
