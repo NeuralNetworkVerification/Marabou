fa60c40d0cb9615938481b0ae3e0eeba8458e1ca
guykatzz <katz911@gmail.com>
2019-01-22 13:39:06 +0200

    cleanup a few cases where the script failed

--- src/engine/Marabou.cpp
+++ src/engine/Marabou.cpp
@@ -14,17 +14,6 @@
  ** [[ Add lengthier description here ]]
  **/
 
-/*! \file Marabou.cpp
- ** \verbatim
- ** Top contributors (to current version):
- **   Guy Katz
- ** This file is part of the Marabou project.
- ** Copyright (c) 2016-2017 by the authors listed in the file AUTHORS
- ** in the top-level source directory) and their institutional affiliations.
- ** All rights reserved. See the file COPYING in the top-level source
- ** directory for licensing information.\endverbatim
- **/
-
 #include "AcasParser.h"
 #include "File.h"
 #include "MStringf.h"
--- src/engine/tests/Test_ConstraintMatrixAnalyzer.h
+++ src/engine/tests/Test_ConstraintMatrixAnalyzer.h
@@ -14,17 +14,6 @@
  ** [[ Add lengthier description here ]]
  **/
 
- /*********************                                                        */
-/*! \file Test_ConstraintMatrixAnalyzer.h
-** \verbatim
-** Top contributors (to current version):
-**   Guy Katz
-** This file is part of the Marabou project.
-** Copyright (c) 2016-2017 by the authors listed in the file AUTHORS
-** in the top-level source directory) and their institutional affiliations.
-** All rights reserved. See the file COPYING in the top-level source
-** directory for licensing information.\endverbatim **/
-
 #include <cxxtest/TestSuite.h>
 
 #include "ConstraintMatrixAnalyzer.h"
