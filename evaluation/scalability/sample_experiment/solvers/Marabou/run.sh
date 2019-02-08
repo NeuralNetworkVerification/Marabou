4648bf63b710132ec56453d75853cb9d2518ccf4
haozewu <haozewu@stanford.edu>
2019-02-07 19:00:30 -0800

    clean up

--- evaluation/scalability/sample_experiment/solvers/Marabou/run.sh
+++ evaluation/scalability/sample_experiment/solvers/Marabou/run.sh
@@ -12,6 +12,6 @@ echo $flags
 trap 'kill -TERM $pid & wait $pid ; sleep 30  ; post' SIGTERM
 
 
-export PYTHONPATH=${PYTHONPATH}:/home/haozewu/Projects/NN/Marabou/
-python3 /home/haozewu/Projects/NN/Marabou/evaluation/scalability/sample_experiment/solvers/Marabou/DnC.py -n $input_net -q $input_property --summary-file=$summary $flags & pid=$!
+export PYTHONPATH=${PYTHONPATH}:/home/cav/marabou/
+python3 /home/cav/marabou/maraboupy/DnC.py -n $input_net -q $input_property --summary-file=$summary $flags & pid=$!
 wait $pid
--- evaluation/scalability/scripts/create_arguments_reluval.py
+++ evaluation/scalability/scripts/create_arguments_reluval.py
@@ -4,7 +4,7 @@ import os
 arguments = []
 
 for filename in os.listdir(sys.argv[1]):
-    tmp_address= "/home/haozewu/Marabou/evaluation/scalability/sample_experiment/tmp/ReluVal/{}_nodes/property4/".format(sys.argv[3])
+    tmp_address= "/home/cav/marabou/evaluation/scalability/sample_experiment/tmp/ReluVal/{}_nodes/property4/".format(sys.argv[3])
     if not os.path.exists(tmp_address):
         os.makedirs(tmp_address)
     if ".nnet" in filename:
