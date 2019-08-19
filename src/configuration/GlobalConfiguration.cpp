93968cad9b01709db2286feaf9a77d43dae74f12
Guy Katz <guykatz@cs.huji.ac.il>
2019-08-19 15:12:54 +0300

    disable by default

--- src/configuration/GlobalConfiguration.cpp
+++ src/configuration/GlobalConfiguration.cpp
@@ -54,7 +54,7 @@ const bool GlobalConfiguration::PREPROCESSOR_ELIMINATE_VARIABLES = true;
 const bool GlobalConfiguration::PREPROCESSOR_PL_CONSTRAINTS_ADD_AUX_EQUATIONS = true;
 const double GlobalConfiguration::PREPROCESSOR_ALMOST_FIXED_THRESHOLD = 0.00001;
 
-const bool GlobalConfiguration::WARM_START = true;
+const bool GlobalConfiguration::WARM_START = false;
 
 const unsigned GlobalConfiguration::PSE_ITERATIONS_BEFORE_RESET = 1000;
 const double GlobalConfiguration::PSE_GAMMA_ERROR_THRESHOLD = 0.001;
