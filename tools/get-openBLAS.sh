3d9a43ab9bd4c24ad719a43b3b9405a15a960f48
haozewu <haozewu@stanford.edu>
2019-09-05 15:47:16 -0700

    small fix

--- tools/get-openBLAS.sh
+++ tools/get-openBLAS.sh
@@ -5,6 +5,6 @@ mv OpenBLAS-0.3.6/ OpenBLAS/
 cd OpenBLAS/
 make NO_SHARED=1 CBLAS_ONLY=1 USE_THREAD=0 -j4
 mkdir installed/
-make PREFIX=installed/ NO_SHARED=0 install
+make PREFIX=installed/ NO_SHARED=1 install
 rm ../v0.3.6.tar.gz
 cd ../../
