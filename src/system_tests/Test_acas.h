940408e11dd3939ba75d12965a70f3806f915215
Yuval Jacoby <yuval999@gmail.com>
2019-10-16 17:50:14 -0700

    port acas tests

--- src/system_tests/Test_acas.h
+++ src/system_tests/Test_acas.h
@@ -35,18 +35,66 @@ public:
     {
     }
 
+    void test_acas_1_1()
+    {
+    }
+
+    void test_acas_2_2_fixed_input()
+    {}
+    /* { */
+    /*     InputQuery inputQuery; */
+    /*     AcasParser acasParser( "../../../resources/nnet/acasxu/ACASXU_experimental_v2a_2_2.nnet" ); */
+    /*     acasParser.generateQuery( inputQuery ); */
+
+    /*     // A simple query: all inputs are fixed to 0 */
+    /*     for ( unsigned i = 0; i < 5; ++i ) */
+    /*     { */
+    /*         unsigned variable = acasParser.getInputVariable( i ); */
+    /*         inputQuery.setLowerBound( variable, i * 0.25 ); */
+    /*         inputQuery.setUpperBound( variable, i * 0.25 ); */
+    /*     } */
+
+    /*     Engine engine; */
+    /*     TS_ASSERT_THROWS_NOTHING( ! engine.processInputQuery( inputQuery ) ); */
+
+    /*     TS_ASSERT_THROWS_NOTHING( engine.solve() ); */
+    /*     engine.extractSolution( inputQuery ); */
+
+    /*     // Run through the original network to check correctness */
+    /*     Vector<double> inputs; */
+    /*     for ( unsigned i = 0; i < 5; ++i ) */
+    /*     { */
+    /*         unsigned variable = acasParser.getInputVariable( i ); */
+    /*         inputs.append( inputQuery.getSolutionValue( variable ) ); */
+    /*     } */
+
+    /*     Vector<double> outputs; */
+    /*     acasParser.evaluate( inputs, outputs ); */
+    /*     double maxError = 0.0; */
+
+    /*     for ( unsigned i = 0; i < 5; ++i ) */
+    /*     { */
+    /*         unsigned variable = acasParser.getOutputVariable( i ); */
+    /*         double newError = FloatUtils::abs( outputs[i] - inputQuery.getSolutionValue( variable ) ); */
+    /*         if ( FloatUtils::gt( newError, maxError ) ) */
+    /*           maxError = newError; */
+    /*     } */
+
+    /*     TS_ASSERT( FloatUtils::lt( maxError, 0.00001 ) ); */
+
+    /* } */
     void test_acas_1_1_fixed_input()
     {
         InputQuery inputQuery;
-        AcasParser acasParser( "./acas_nnet/ACASXU_run2a_1_1_batch_2000.nnet" );
+        AcasParser acasParser( "../../../resources/nnet/acasxu/ACASXU_experimental_v2a_1_1.nnet" );
         acasParser.generateQuery( inputQuery );
 
         // A simple query: all inputs are fixed to 0
         for ( unsigned i = 0; i < 5; ++i )
         {
             unsigned variable = acasParser.getInputVariable( i );
-            inputQuery.setLowerBound( variable, 0.0 );
-            inputQuery.setUpperBound( variable, 0.0 );
+            inputQuery.setLowerBound( variable, -0.01 );
+            inputQuery.setUpperBound( variable, 0.00 );
         }
 
         Engine engine;
