7cb0d745b8964d0f953d2767dd18eef3aa30e282
kjulian3 <kjulian3@stanford.edu>
2019-10-25 15:54:51 -0700

    Fix spacing and order of includes

--- src/query_loader/QueryLoader.cpp
+++ src/query_loader/QueryLoader.cpp
@@ -2,7 +2,7 @@
 /*! \file QueryLoader.cpp
  ** \verbatim
  ** Top contributors (to current version):
- **   Christopher Lazarus
+ **   Christopher Lazarus, Kyle Julian
  ** This file is part of the Marabou project.
  ** Copyright (c) 2017-2019 by the authors listed in the file AUTHORS
  ** in the top-level source directory) and their institutional affiliations.
@@ -17,19 +17,19 @@
 #include "Debug.h"
 #include "Equation.h"
 #include "File.h"
+#include "GlobalConfiguration.h"
 #include "InputQuery.h"
-#include "MStringf.h"
+#include "MarabouError.h"
 #include "MaxConstraint.h"
+#include "MStringf.h"
 #include "QueryLoader.h"
 #include "ReluConstraint.h"
-#include "GlobalConfiguration.h"
-#include "MarabouError.h"
 
 InputQuery QueryLoader::loadQuery( const String &fileName )
 {
     if ( !File::exists( fileName ) )
     {
-        throw MarabouError(MarabouError::FILE_DOES_NOT_EXIST, Stringf( "File %s not found.\n", fileName.ascii() ).ascii() );
+        throw MarabouError( MarabouError::FILE_DOES_NOT_EXIST, Stringf( "File %s not found.\n", fileName.ascii() ).ascii() );
     }
 
     InputQuery inputQuery;
@@ -42,17 +42,17 @@ InputQuery QueryLoader::loadQuery( const String &fileName )
     unsigned numEquations = atoi( input.readLine().trim().ascii() );
     unsigned numConstraints = atoi( input.readLine().trim().ascii() );
 
-    log(Stringf("Number of variables: %u\n", numVars));
-    log(Stringf("Number of lower bounds: %u\n", numLowerBounds));
-    log(Stringf("Number of upper bounds: %u\n", numUpperBounds));
-    log(Stringf("Number of equations: %u\n", numEquations));
-    log(Stringf("Number of constraints: %u\n", numConstraints));
+    log( Stringf( "Number of variables: %u\n", numVars ) );
+    log( Stringf( "Number of lower bounds: %u\n", numLowerBounds ) );
+    log( Stringf( "Number of upper bounds: %u\n", numUpperBounds ) );
+    log( Stringf( "Number of equations: %u\n", numEquations ) );
+    log( Stringf( "Number of constraints: %u\n", numConstraints ) );
 
     inputQuery.setNumberOfVariables( numVars );
 
     // Input Variables
     unsigned numInputVars = atoi( input.readLine().trim().ascii() );
-    for (unsigned i = 0; i < numInputVars; ++i)
+    for ( unsigned i = 0; i < numInputVars; ++i )
     {
         String line = input.readLine();
         List<String> tokens = line.tokenize( "," );
@@ -65,8 +65,8 @@ InputQuery QueryLoader::loadQuery( const String &fileName )
     }
 
     // Output Variables
-    unsigned numOutputVars = atoi(input.readLine().trim().ascii());
-    for (unsigned i = 0; i < numOutputVars; ++i)
+    unsigned numOutputVars = atoi( input.readLine().trim().ascii() );
+    for ( unsigned i = 0; i < numOutputVars; ++i )
     {
         String line = input.readLine();
         List<String> tokens = line.tokenize( "," );
@@ -86,7 +86,7 @@ InputQuery QueryLoader::loadQuery( const String &fileName )
         List<String> tokens = line.tokenize( "," );
 
         // format: <var, lb>
-        ASSERT(tokens.size() == 2);
+        ASSERT( tokens.size() == 2 );
 
         auto it = tokens.begin();
         unsigned varToBound = atoi( it->ascii() );
@@ -121,23 +121,23 @@ InputQuery QueryLoader::loadQuery( const String &fileName )
     }
 
     // Equations
-    for( unsigned i = 0; i< numEquations; ++i )
+    for( unsigned i = 0; i < numEquations; ++i )
     {
-        log(Stringf("Equation: %u ", i));
+        log( Stringf( "Equation: %u ", i ) );
         String line = input.readLine();
 
         List<String> tokens = line.tokenize( "," );
-        ASSERT(tokens.size() > 4);
+        ASSERT( tokens.size() > 4 );
         
         auto it = tokens.begin();
 
         // Skip equation number
         ++it;
         int eqType = atoi( it->ascii() );
-        log(Stringf("Type: %u ", eqType));
+        log( Stringf("Type: %u ", eqType ) );
         ++it;
         double eqScalar = atof( it->ascii() );
-        log(Stringf("Scalar: %f\n", eqScalar));
+        log( Stringf( "Scalar: %f\n", eqScalar ) );
 
         Equation::EquationType type = Equation::EQ;
 
@@ -157,7 +157,7 @@ InputQuery QueryLoader::loadQuery( const String &fileName )
 
         default:
             // Throw exception
-            throw MarabouError(MarabouError::INVALID_EQUATION_TYPE, Stringf("Invalid Equation Type\n").ascii() );
+            throw MarabouError( MarabouError::INVALID_EQUATION_TYPE, Stringf( "Invalid Equation Type\n" ).ascii() );
             break;
         }
 
@@ -171,7 +171,7 @@ InputQuery QueryLoader::loadQuery( const String &fileName )
             ASSERT( it != tokens.end() );
             double coeff = atof( it->ascii() );
 
-            log(Stringf("\tVar_no: %i, Coeff: %f\n", varNo, coeff));
+            log( Stringf( "\tVar_no: %i, Coeff: %f\n", varNo, coeff ) );
 
             equation.addAddend( coeff, varNo );
         }
@@ -193,14 +193,14 @@ InputQuery QueryLoader::loadQuery( const String &fileName )
         String serializeConstraint;
         // include type in serializeConstraint as well
         while ( it != tokens.end() ) {
-            serializeConstraint += *it + String(",");
+            serializeConstraint += *it + String( "," );
             it++;
         }
-        serializeConstraint = serializeConstraint.substring(0, serializeConstraint.length()-1);
+        serializeConstraint = serializeConstraint.substring( 0, serializeConstraint.length() - 1 );
 
         PiecewiseLinearConstraint *constraint = NULL;
-        log(Stringf("Constraint: %u, Type: %s \n", i, coType.ascii()));
-        log(Stringf("\tserialized:\t%s \n", serializeConstraint.ascii()));
+        log( Stringf( "Constraint: %u, Type: %s \n", i, coType.ascii() ) );
+        log( Stringf( "\tserialized:\t%s \n", serializeConstraint.ascii() ) );
         if ( coType == "relu" )
         {
             constraint = new ReluConstraint( serializeConstraint );
@@ -211,7 +211,7 @@ InputQuery QueryLoader::loadQuery( const String &fileName )
         }
         else
         {
-            throw MarabouError(MarabouError::UNSUPPORTED_PIECEWISE_CONSTRAINT, Stringf("UNSUPPORTED PIECEWISE CONSTRAINT: %s\n", coType.ascii()).ascii() );
+            throw MarabouError( MarabouError::UNSUPPORTED_PIECEWISE_CONSTRAINT, Stringf( "UNSUPPORTED PIECEWISE CONSTRAINT: %s\n", coType.ascii() ).ascii() );
         }
 
         ASSERT( constraint );
