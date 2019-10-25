828fc6e7314091d8ae36eaab5b6628eefd49b194
kjulian3 <kjulian3@stanford.edu>
2019-10-25 15:41:58 -0700

    Update Marabou.py

--- maraboupy/Marabou.py
+++ maraboupy/Marabou.py
@@ -72,9 +72,9 @@ def solve_query(ipq, filename="", verbose=True, timeout=0, verbosity=2):
         timeout: (int) time in seconds when Marabou will time out
         verbose: (bool) whether to print out solution after solve finishes
         verbosity: (int) determines how much Marabou prints during solving
-                0: No printing
-                1: Some printing
-                2 (default): Standard printing
+                0: print out minimal information
+                1: print out statistics only in the beginning and the end
+                2: print out statistics during solving
     Returns:
         vals: (dict: int->float) empty if UNSAT, else SATisfying solution
         stats: (Statistics) a Statistics object as defined in Marabou,
