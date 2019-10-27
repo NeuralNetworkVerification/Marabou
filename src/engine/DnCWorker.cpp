6f70866936604515efa8ae2509c2726cdd9f4ec1
Haoze(Andrew) Wu <haozewu@stanford.edu>
2019-10-27 08:03:28 -0700

    Unit test for DnCWorker (#194)
    * unit test
    
    * move ExitCode to IEngine
    
    * mock engine
    
    * unit test for dnc worker
    
    * clean up
    
    * cosmetics
    
    * dncworker
    
    * fix timeout
    
    * fix bug
    
    * polishing the unit test
    
    * polishing
    
    * small fixes
    
    * not resetting entry strategy
    
    * small change
    
    * test dncworker
    
    * clean up
    
    * clean up
    
    * UNDEF header

--- src/engine/DnCWorker.cpp
+++ src/engine/DnCWorker.cpp
@@ -16,9 +16,10 @@
 #include "Debug.h"
 #include "DivideStrategy.h"
 #include "DnCWorker.h"
-#include "Engine.h"
+#include "IEngine.h"
 #include "EngineState.h"
 #include "LargestIntervalDivider.h"
+#include "MarabouError.h"
 #include "MStringf.h"
 #include "PiecewiseLinearCaseSplit.h"
 #include "SubQuery.h"
@@ -28,7 +29,7 @@
 #include <cmath>
 #include <thread>
 
-DnCWorker::DnCWorker( WorkerQueue *workload, std::shared_ptr<Engine> engine,
+DnCWorker::DnCWorker( WorkerQueue *workload, std::shared_ptr<IEngine> engine,
                       std::atomic_uint &numUnsolvedSubQueries,
                       std::atomic_bool &shouldQuitSolving,
                       unsigned threadId, unsigned onlineDivides,
@@ -60,132 +61,123 @@ void DnCWorker::setQueryDivider( DivideStrategy divideStrategy )
     }
 }
 
-void DnCWorker::run()
+void DnCWorker::popOneSubQueryAndSolve()
 {
-    while ( _numUnsolvedSubQueries->load() > 0 )
+    SubQuery *subQuery = NULL;
+    // Boost queue stores the next element into the passed-in pointer
+    // and returns true if the pop is successful (aka, the queue is not empty
+    // in most cases)
+    if ( _workload->pop( subQuery ) )
     {
-        SubQuery *subQuery = NULL;
-        // Boost queue stores the next element into the passed-in pointer
-        // and returns true if the pop is successful (aka, the queue is not empty
-        // in most cases)
-        if ( _workload->pop( subQuery ) )
+        String queryId = subQuery->_queryId;
+        auto split = std::move( subQuery->_split );
+        unsigned timeoutInSeconds = subQuery->_timeoutInSeconds;
+
+        // Reset the engine state
+        _engine->restoreState( *_initialState );
+        _engine->reset();
+
+        // TODO: each worker is going to keep a map from *CaseSplit to an
+        // object of class DnCStatistics, which contains some basic
+        // statistics. The maps are owned by the DnCManager.
+
+        // Apply the split and solve
+        _engine->applySplit( *split );
+        _engine->solve( timeoutInSeconds );
+
+        IEngine::ExitCode result = _engine->getExitCode();
+        printProgress( queryId, result );
+        // Switch on the result
+        if ( result == IEngine::UNSAT )
         {
-            String queryId = subQuery->_queryId;
-            auto split = std::move( subQuery->_split );
-            unsigned timeoutInSeconds = subQuery->_timeoutInSeconds;
-
-            // Create a new statistics object for each subQuery
-            Statistics *statistics = new Statistics();
-            _engine->resetStatistics( *statistics );
-            // Reset the engine state
-            _engine->restoreState( *_initialState );
-            _engine->clearViolatedPLConstraints();
-            _engine->resetSmtCore();
-            _engine->resetBoundTighteners();
-            _engine->resetExitCode();
-            // TODO: each worker is going to keep a map from *CaseSplit to an
-            // object of class DnCStatistics, which contains some basic
-            // statistics. The maps are owned by the DnCManager.
-
-            // Apply the split and solve
-            _engine->applySplit( *split );
-            _engine->solve( timeoutInSeconds );
-
-            Engine::ExitCode result = _engine->getExitCode();
-            printProgress( queryId, result );
-            // Switch on the result
-            if ( result == Engine::UNSAT )
+            // If UNSAT, continue to solve
+            *_numUnsolvedSubQueries -= 1;
+            if ( _numUnsolvedSubQueries->load() == 0 )
+                *_shouldQuitSolving = true;
+            delete subQuery;
+        }
+        else if ( result == IEngine::TIMEOUT )
+        {
+            // If TIMEOUT, split the current input region and add the
+            // new subQueries to the current queue
+            SubQueries subQueries;
+            _queryDivider->createSubQueries( pow( 2, _onlineDivides ),
+                                             queryId, *split,
+                                             (unsigned)timeoutInSeconds *
+                                             _timeoutFactor, subQueries );
+            for ( auto &newSubQuery : subQueries )
             {
-                // If UNSAT, continue to solve
-                *_numUnsolvedSubQueries -= 1;
-                delete subQuery;
-            }
-            else if ( result == Engine::TIMEOUT )
-            {
-                // If TIMEOUT, split the current input region and add the
-                // new subQueries to the current queue
-                SubQueries subQueries;
-                _queryDivider->createSubQueries( pow( 2, _onlineDivides ),
-                                                 queryId, *split,
-                                                 (unsigned) timeoutInSeconds *
-                                                 _timeoutFactor, subQueries );
-                for ( auto &newSubQuery : subQueries )
+                if ( !_workload->push( std::move( newSubQuery ) ) )
                 {
-                    if ( !_workload->push( std::move( newSubQuery ) ) )
-                    {
-                        ASSERT( false );
-                    }
-
-                    *_numUnsolvedSubQueries += 1;
+                    throw MarabouError( MarabouError::UNSUCCESSFUL_QUEUE_PUSH );
                 }
-                *_numUnsolvedSubQueries -= 1;
-                delete subQuery;
+
+                *_numUnsolvedSubQueries += 1;
             }
-            else if ( result == Engine::SAT )
-            {
-                // If SAT, set the shouldQuitSolving flag to true, so that the
-                // DnCManager will kill all the DnCWorkers
-                *_shouldQuitSolving = true;
-                *_numUnsolvedSubQueries -= 1;
-                delete subQuery;
-                return;
-            }
-            else if ( result == Engine::QUIT_REQUESTED )
-            {
-                // If engine was asked to quit, quit
-                std::cout << "Quit requested by manager!" << std::endl;
-                *_numUnsolvedSubQueries -= 1;
-                delete subQuery;
-                return;
-            }
-            else if ( result == Engine::ERROR )
-            {
-                // If ERROR, set the shouldQuitSolving flag to true and quit
-                std::cout << "Error!" << std::endl;
-                *_shouldQuitSolving = true;
-                *_numUnsolvedSubQueries -= 1;
-                delete subQuery;
-                return;
-            }
-            else if ( result == Engine::NOT_DONE )
-            {
-                // If NOT_DONE, set the shouldQuitSolving flag to true and quit
-                ASSERT( false );
-                std::cout << "Not done! This should not happen." << std::endl;
-                *_shouldQuitSolving = true;
-                *_numUnsolvedSubQueries -= 1;
-                delete subQuery;
-                return;
-            }
+            *_numUnsolvedSubQueries -= 1;
+            delete subQuery;
+        }
+        else if ( result == IEngine::QUIT_REQUESTED )
+        {
+            // If engine was asked to quit, quit
+            std::cout << "Quit requested by manager!" << std::endl;
+            delete subQuery;
+            ASSERT( _shouldQuitSolving->load() );
         }
         else
         {
-            // If the queue is empty but the pop fails, wait and retry
-            std::this_thread::sleep_for( std::chrono::milliseconds( 100 ) );
+            // We must set the quit flag to true  if the result is not UNSAT or
+            // TIMEOUT. This way, the DnCManager will kill all the DnCWorkers.
+
+            *_shouldQuitSolving = true;
+            if ( result == IEngine::SAT )
+            {
+                // case SAT
+                *_numUnsolvedSubQueries -= 1;
+                delete subQuery;
+            }
+            else if ( result == IEngine::ERROR )
+            {
+                // case ERROR
+                std::cout << "Error!" << std::endl;
+                delete subQuery;
+            }
+            else // result == IEngine::NOT_DONE
+            {
+                // case NOT_DONE
+                ASSERT( false );
+                std::cout << "Not done! This should not happen." << std::endl;
+                delete subQuery;
+            }
         }
+    }
+    else
+    {
+        // If the queue is empty but the pop fails, wait and retry
+        std::this_thread::sleep_for( std::chrono::milliseconds( 100 ) );
     }
 }
 
-void DnCWorker::printProgress( String queryId, Engine::ExitCode result ) const
+void DnCWorker::printProgress( String queryId, IEngine::ExitCode result ) const
 {
     printf( "Worker %d: Query %s %s, %d tasks remaining\n", _threadId,
             queryId.ascii(), exitCodeToString( result ).ascii(),
             _numUnsolvedSubQueries->load() );
 }
 
-String DnCWorker::exitCodeToString( Engine::ExitCode result )
+String DnCWorker::exitCodeToString( IEngine::ExitCode result )
 {
     switch ( result )
     {
-    case Engine::UNSAT:
+    case IEngine::UNSAT:
         return "UNSAT";
-    case Engine::SAT:
+    case IEngine::SAT:
         return "SAT";
-    case Engine::ERROR:
+    case IEngine::ERROR:
         return "ERROR";
-    case Engine::TIMEOUT:
+    case IEngine::TIMEOUT:
         return "TIMEOUT";
-    case Engine::QUIT_REQUESTED:
+    case IEngine::QUIT_REQUESTED:
         return "QUIT_REQUESTED";
     default:
         ASSERT( false );
