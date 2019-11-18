f07b36dc995d470b701ba3f41931dd7579e6619d
haozewu <haozewu@stanford.edu>
2019-11-17 16:52:48 -0800

    relu divider

---
+++ src/engine/ReluDivider.h
@@ -0,0 +1,62 @@
+/*********************                                                        */
+/*! \file ReluLookAheadDivider.h
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Haoze Wu
+ ** This file is part of the Marabou project.
+ ** Copyright (c) 2017-2019 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved. See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** [[ Add lengthier description here ]]
+
+**/
+
+#ifndef __ReluDivider_h__
+#define __ReluDivider_h__
+
+#include "IEngine.h"
+#include "List.h"
+#include "PiecewiseLinearConstraint.h"
+#include "QueryDivider.h"
+
+#include <math.h>
+
+class ReluDivider : public QueryDivider
+{
+public:
+    ReluDivider( std::shared_ptr<IEngine> engine );
+
+    void createSubQueries( unsigned numNewSubQueries,
+                           const String queryIdPrefix,
+                           const PiecewiseLinearCaseSplit
+                           &previousSplit,
+                           const unsigned timeoutInSeconds,
+                           SubQueries &subQueries );
+
+    /*
+      Returns the variable with the largest range
+    */
+    PiecewiseLinearConstraint *getPLConstraintToSplit( const
+                                                       PiecewiseLinearCaseSplit
+                                                       &split );
+
+    PiecewiseLinearConstraint *computeBestChoice();
+
+private:
+    /*
+      The engine
+    */
+    std::shared_ptr<IEngine> _engine;
+};
+
+#endif // __ReluDivider_h__
+
+//
+// Local Variables:
+// compile-command: "make -C ../.. "
+// tags-file-name: "../../TAGS"
+// c-basic-offset: 4
+// End:
+//
