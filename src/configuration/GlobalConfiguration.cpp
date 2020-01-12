8c7e782658eb091754116b47246d01a4f59f1eba
Andrew Wu <haozewu@barrett2.stanford.edu>
2020-01-12 13:46:22 -0800

    tune parameter

--- src/configuration/GlobalConfiguration.cpp
+++ src/configuration/GlobalConfiguration.cpp
@@ -19,7 +19,7 @@
 
 const unsigned GlobalConfiguration::QUICK_SOLVE_STACK_DEPTH_THRESHOLD = 6;
 const unsigned GlobalConfiguration::CONSTRAINT_VIOLATION_THRESHOLD_QUICK = 20;
-const unsigned GlobalConfiguration::RUNTIME_ESTIMATE_THRESHOLD = 20;
+const unsigned GlobalConfiguration::RUNTIME_ESTIMATE_THRESHOLD = 40;
 const double GlobalConfiguration::DEFAULT_EPSILON_FOR_COMPARISONS = 0.0000000001;
 const unsigned GlobalConfiguration::DEFAULT_DOUBLE_TO_STRING_PRECISION = 10;
 const unsigned GlobalConfiguration::STATISTICS_PRINTING_FREQUENCY = 10000;
