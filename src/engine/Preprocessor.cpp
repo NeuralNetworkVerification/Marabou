f2a93ef478e173be294168e6b33e7495df797273
Guy Katz <katz911@gmail.com>
2020-03-28 15:41:31 +0300

    support for eliminating unused variables

--- src/engine/Preprocessor.cpp
+++ src/engine/Preprocessor.cpp
@@ -494,10 +494,47 @@ bool Preprocessor::processIdenticalVariables()
 
 void Preprocessor::collectFixedValues()
 {
+    // Compute all used variables:
+    //   1. Variables that appear in equations
+    //   2. Variables that participate in PL constraints
+    //   3. Variables that have been merged (and hence, previously
+    //      appeared in an equation)
+    Set<unsigned> usedVariables;
+    for ( const auto &equation : _preprocessed.getEquations() )
+        usedVariables += equation.getParticipatingVariables();
+    for ( const auto &constraint : _preprocessed.getPiecewiseLinearConstraints() )
+    {
+        for ( const auto &var : constraint->getParticipatingVariables() )
+            usedVariables.insert( var );
+    }
+    for ( const auto &merged : _mergedVariables )
+        usedVariables.insert( merged.first );
+
+    // Collect any variables with identical lower and upper bounds, or
+    // which are unused
 	for ( unsigned i = 0; i < _preprocessed.getNumberOfVariables(); ++i )
 	{
         if ( FloatUtils::areEqual( _preprocessed.getLowerBound( i ), _preprocessed.getUpperBound( i ) ) )
+        {
             _fixedVariables[i] = _preprocessed.getLowerBound( i );
+        }
+        else if ( !usedVariables.exists( i ) )
+        {
+            printf( "Have an unused var: x%u\n", i );
+
+            // If possible, choose a value that matches the debugging
+            // solution. Otherwise, pick the lower bound
+            if ( _preprocessed._debuggingSolution.exists( i ) &&
+                 _preprocessed._debuggingSolution[i] >= _preprocessed.getLowerBound( i ) &&
+                 _preprocessed._debuggingSolution[i] <= _preprocessed.getUpperBound( i ) )
+            {
+                _fixedVariables[i] = _preprocessed._debuggingSolution[i];
+            }
+            else
+            {
+                _fixedVariables[i] = _preprocessed.getLowerBound( i );
+            }
+        }
 	}
 }
 
