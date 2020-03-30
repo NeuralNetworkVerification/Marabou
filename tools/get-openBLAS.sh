a9006187e68d3d8fef6a4ec8336f45eeaf975fe0
Guy Katz <katz911@gmail.com>
2020-03-30 11:12:41 +0300

    newer openblas version

--- tools/get-openBLAS.sh
+++ tools/get-openBLAS.sh
@@ -5,13 +5,14 @@ mydir="${0%/*}"
 cd $mydir
 
 echo "downloding openBLAS"
-wget http://github.com/xianyi/OpenBLAS/archive/v0.3.6.tar.gz
+wget http://github.com/xianyi/OpenBLAS/archive/v0.3.9.tar.gz
 echo "unzipping openBLAS"
-tar -xzf v0.3.6.tar.gz >> /dev/null
+tar -xzf v0.3.9.tar.gz >> /dev/null
 echo "installing openBLAS"
-mv OpenBLAS-0.3.6/ OpenBLAS/
-cd OpenBLAS/
-make NO_SHARED=1 CBLAS_ONLY=1 USE_THREAD=0 -j4 >> /dev/null
+cd OpenBLAS-0.3.9/
+make NO_SHARED=1 CBLAS_ONLY=1 USE_THREAD=0 >> /dev/null
 mkdir installed/
-make PREFIX=installed/ NO_SHARED=1 install >> /dev/null
+make PREFIX=installed NO_SHARED=1 install >> /dev/null
 cd $curdir
+
+## TODO: open blas is in single thread mode. Is this what reluval uses?
