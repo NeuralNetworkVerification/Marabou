2f6fcb69f8e474c245931e75a5d59357edf72666
Matan Ostrovsky <matanost@gmail.com>
2020-04-16 00:49:40 +0300

    Update

--- maraboupy/MarabouParseCnn.py
+++ maraboupy/MarabouParseCnn.py
@@ -34,205 +34,9 @@ def l2str(layer_i):
     while layer_i > 0:
         out_str = chr((layer_i % N) + ord('a')) + out_str
         return out_str
-    
-def n2str(layer_i,node_i):
-    return l2str(layer_i) + str(node_i)
 
-class Filter1D:    
-    def __init__(self, weights):
-        self.window_size = len(weights)
-        self.weights = weights
-
-class Cnn(nx.DiGraph):
-
-    red = '#ff293e'
-    blue = '#1f78b4'
-    #-------------Graphics: print the network-------------#
-    
-    def draw_layer(self,layer):      
-        self.layout.update({n : ((i * self.draw_scale), - self.next_lyr_pos* self.draw_scale) for i,n in enumerate(layer)})
-        self.next_lyr_pos += self.draw_scale
-
-    def cnn_layout(self):
-        return self.layout
-    
-    def print_to_file(self,file, mark=[]):
-        fog_x_w = 0.7 * len(self.in_l)
-        fig_y_w = 0.7 * max(self.l_num, ceil( 1 * self.l_num))#(2/3)
-        plt.figure(figsize=(fog_x_w, fig_y_w), dpi=100)
-        labels = {n : n for n in self.nodes()}
-        if mark:
-            node_colors = [Cnn.red if n in mark else Cnn.blue for n in self.nodes()]
-        else :
-            node_colors = Cnn.blue       
-        nx.draw_networkx(self, pos=self.cnn_layout(), with_labels=True, labels=labels, node_color=node_colors)
-        plt.savefig(file)    
-
-    def __str__(self):
-        out = ""
-        for u,v,w in self.edges.data('weight'):
-            out = out + "({},{}), w:={}".format(u,v,w)+ "\n"
-        return out
-
-    #-------------Add filters and layers-------------#
-
-    def add_filter(self, f):
-        new_l = list()
-        self.l_num += 1
-        for i in range(len(self.out_l) - f.window_size + 1):
-            act_n = n2str(self.l_num, i)
-            self.add_node(act_n)
-            new_l.append(act_n) 
-            for j, w in enumerate(f.weights):
-                self.add_edge(self.out_l[i + j], act_n, weight=w)
-        self.out_l = new_l
-        self.draw_layer(new_l)
-
-    def add_layer(self, w_dict): #Assume some element will be added. in the form of int->int dict.
-        self.l_num += 1
-        new_l = []
-        max_node_new_l = -1
-        new_l = list()
-        for i in range(len(self.out_l)):
-            new_wn = w_dict[i]
-            if len(new_wn) == 0:
-                continue
-            max_n_ind = max(nw[0] for nw in new_wn)
-            if max_node_new_l < max_n_ind:
-                last_max = max_node_new_l
-                max_node_new_l = max_n_ind
-                for j in range(last_max + 1, max_node_new_l + 1):
-                    self.add_node(n2str(self.l_num, j))
-                    new_l.append(n2str(self.l_num, j))
-            i_str = n2str(self.l_num - 1, i)
-            #print("For i={} we have list={}".format(i,new_wn))
-            for wn in new_wn:
-                n_str = n2str(self.l_num, wn[0])
-                w = wn[1]
-                self.add_edge(i_str, n_str, weight=w)
-        self.out_l = new_l
-        self.draw_layer(new_l)
-
-    #-------------Solve the network-------------#        
-        
-    def solve(self, in_prop, out_prop):
-        #example:
-        #in_prop = {n : (-mnx.large, mnx.large) for n in cnn.in_l}
-        #out_prop = {n : (-mnx.large, mnx.large) for n in cnn.out_l}
-        iq = mnx.networkxToInputQuery(self, in_prop, out_prop)
-
-        print("Start solving")
-        vars1, stats1 = MarabouCore.solve(iq, Marabou.createOptions())
-        print("Finish solving")
-        
-        if len(vars1)>0:
-            print("SAT")
-            print(vars1)
-        else:
-            print("UNSAT")
-                
-    #-------------Init-------------#            
-
-    def __init__(self, in_l_size):
-        super().__init__()
-        self.out_l = list() # L is for layer.        
-        self.in_l = list()  # L is for layer.
-        self.l_num = 0  # L is for layer.
-        self.next_lyr_pos = 0
-        self.layout = dict()
-        self.draw_scale = 1
-        for i in range(in_l_size):
-            new_n = n2str(0,i)
-            self.add_node(new_n)
-            self.out_l.append(new_n)
-            self.in_l.append(new_n)
-        self.draw_layer(self.in_l)
-
-    #-------------Init from existing NX-------------# 
-
-    def init_from_nx_DiGraph(di_graph):
-        cnn = Cnn(0)
-        cnn.clear()
-        cnn.add_nodes_from(di_graph.nodes())
-        cnn.add_edges_from(di_graph.edges())
-        cnn.in_l = set() 
-        cnn.out_l = set()       
-        for n in cnn.nodes():
-            if not cnn.in_edges(n):
-                cnn.in_l.add(n)
-            if not cnn.out_edges(n):
-                cnn.out_l.add(n)                
-        cnn.l_num = 0  # L is for layer. TODO
-        cnn.next_lyr_pos = 0
-        cnn.layout = dict()
-        visited = set()
-        to_visit = cnn.in_l
-        while to_visit:
-            cnn.draw_layer(to_visit)
-            cnn.l_num += 1
-            visited = set.union(visited, to_visit)
-            to_visit = set.difference(set([s for s in [u for u in to_visit]]), visited)
-        return cnn   
-
-    #-------------Find the Cone of Influence of a set of vertices-------------#
-
-    def coi(graph, vertices): # Cone of Influencers
-        ancestors = set()
-        descendants = set()
-        for vertex in vertices:
-            ancestors = set.union(ancestors, nx.algorithms.dag.ancestors(graph, vertex)) 
-            descendants = set.union(descendants, nx.algorithms.dag.descendants(graph, vertex))
-        graph_copy = copy.deepcopy(graph)
-        for u in graph:
-                if (u not in ancestors) and (u not in vertices) and (u not in descendants):
-                    graph_copy.remove_node(u)
-        return graph_copy
-
-    #-------------TF to NetworkX-------------#
-    
-    def get_session(filename):
-        with tf.compat.v1.gfile.GFile(filename, "rb") as f:
-            graph_def = tf.compat.v1.GraphDef()
-            graph_def.ParseFromString(f.read())
-        with tf.Graph().as_default() as graph:
-            tf.import_graph_def(graph_def, name="")
-        return  tf.compat.v1.Session(graph=graph)
-
-    def get_operations(sess):
-        return sess.graph.get_operations()
-        
-    #-------------Keras to TF-------------#
-    #https://www.dlology.com/blog/how-to-convert-trained-keras-model-to-tensorflow-and-make-prediction/
-    def freeze_session(session, keep_var_names=None, output_names=None, clear_devices=True):
-        graph = session.graph
-        with graph.as_default():
-            freeze_var_names = list(set(v.op.name for v in tf.compat.v1.global_variables()).difference(keep_var_names or []))
-            output_names = output_names or []
-            output_names += [v.op.name for v in tf.compat.v1.global_variables()]
-            # Graph -> GraphDef ProtoBuf
-            input_graph_def = graph.as_graph_def()
-            if clear_devices:
-                for node in input_graph_def.node:
-                    node.device = ""
-            frozen_graph = convert_variables_to_constants(session, input_graph_def, output_names, freeze_var_names)
-        return frozen_graph
-    #Use:
-    #frozen_graph = freeze_session(backend.get_session(), output_names=[out.op.name for out in model.outputs]) 
-
-    def save_frozen_graph(file_name, frozen_graph):
-        # Save to ./model/tf_model.pb
-        tf.train.write_graph(frozen_graph, "model", file_name, as_text=False)
-
-    def save_keras_model_to_pb(model, file_name):
-        frozen_graph = Cnn.freeze_session(backend.get_session(), output_names=[out.op.name for out in model.outputs])
-        Cnn.save_frozen_graph(file_name, frozen_graph)
-
-###############################################################################################################
-###############################################################################################################
-###############################################################################################################
-        
 def n2str_md(layer_i,node_cor):
-    return l2str(layer_i) + str(node_cor)
+    return l2str(layer_i) + "#" + "".join(["%{}".format(c) for c in node_cor])
 
 class Filter:    
     def __init__(self, weights, bias=None, function="Relu", shape=None):                    
@@ -247,8 +51,11 @@ class Filter:
             self.dim["y"] = weights.shape[1]
             self.dim["d"] = weights.shape[2] #Input depth
             self.dim["f"] = weights.shape[3] #Filter amount
+            print("DIM ={}".format(self.dim))
             self.weights = weights
             self.bias = bias
+            print(self.bias.shape)            
+            print(self.bias)
         elif function is "MaxPool":
             self.dim = dict()
             self.dim["x"] = shape[0]
@@ -314,9 +121,12 @@ class Cnn2D(nx.DiGraph):
                     #    raise Exception("Relu: Filter and layer depth should be equal")
                     for f_i in range(f.dim["f"]):
                         act_n = n2str_md(self.l_num, [x_i, y_i, f_i])
-                        if any([dim > 1 for dim in f.bias[f_i].shape]):
-                            raise Exception("Bias should be a scalar. Bias =\n{}\n".format(f.bias[f_i]))
-                        self.add_node(act_n, function=f.function, bias=f.bias[f_i])
+                        
+                        ##FIXME should address bias.
+                        ##if any([dim > 1 for dim in f.bias[f_i].shape]):                        
+                            ###raise Exception("Bias should be a scalar. Bias =\n{}\n".format(f.bias[f_i]))
+                        ##self.add_node(act_n, function=f.function, bias=f.bias[f_i])
+                        self.add_node(act_n, function=f.function, bias=0) #TODO replace with above
                         new_l[(x_i, y_i, f_i)] = act_n 
                         for x_j, y_j, d_j in itertools.product(range(f.dim["x"]),range(f.dim["y"]),range(f.dim["d"])):
                             self.add_edge(self.out_l[(x_cor + x_j, y_cor + y_j, d_j)], act_n, weight=f.weights[x_j][y_j][d_j][f_i])
--- maraboupy/examples/test_cnn2D.py
+++ maraboupy/examples/test_cnn2D.py
@@ -138,7 +138,7 @@ def train_cnn2D():
     ##################
     
     model = Sequential()
-    model.add(Conv2D(5, (3, 3), activation='relu', input_shape=input_shape, use_bias=bias_active))
+    model.add(Conv2D(5, (3, 4), activation='relu', input_shape=input_shape, use_bias=bias_active))
     model.add(MaxPooling2D((2, 2)))
     model.add(Conv2D(5, (2, 2), activation='relu', use_bias=bias_active))
     model.add(MaxPooling2D((2, 2)))
@@ -149,40 +149,37 @@ def train_cnn2D():
     ###################
     ####Fit
     ###################
-    
-    #model.compile(loss=keras.losses.categorical_crossentropy,
-    #              optimizer=keras.optimizers.Adadelta(),
-    #              metrics=['accuracy'])
 
-    model.compile(optimizer='adam',
-              loss=keras.losses.categorical_crossentropy,
-              metrics=['accuracy'])
+    #model.compile(optimizer='adam', TODO
+    #          loss=keras.losses.categorical_crossentropy,
+    #          metrics=['accuracy'])
     
-    history = model.fit(train_images, train_labels, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(test_images, test_labels))
+    #history = model.fit(train_images, train_labels, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(test_images, test_labels)) TODO
     
     ###################
     ####Evaluate
     ###################
     
-    print(history.history)
-    plt.plot(history.history['accuracy'], label='accuracy')
-    plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
-    plt.xlabel('Epoch')
-    plt.ylabel('Accuracy')
-    plt.ylim([0.5, 1])
-    plt.legend(loc='lower right')
+    #print(history.history) TODO
+    #plt.plot(history.history['accuracy'], label='accuracy')
+    #plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+    #plt.xlabel('Epoch')
+    #plt.ylabel('Accuracy')
+    #plt.ylim([0.5, 1])
+    #plt.legend(loc='lower right')
     #plt.show() #TODO FIXME save the figure.
     
-    test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
-    print('Test loss:', test_loss)
-    print('Test accuracy:', test_acc)
-    model.summary()
-    model.save(file_name)
+    #test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+    #print('Test loss:', test_loss)
+    #print('Test accuracy:', test_acc)
+    #model.summary()
+    #model.save(file_name)
 
-    rand_im = rand = np.random.random([x,y,d])
-    rand_label = model.predict(np.array([rand]))
-    
-     in_l_size = {"x":x , "y":y, "d":d }
+    #rand_im = np.random.random([x,y,d])
+    #rand_label = model.predict(np.array([rand]))
+    rand_im = None
+    rand_label = None
+    in_l_size = {"x":x , "y":y, "d":d }
 
     ###################
     ####Return CNN Obj
@@ -217,72 +214,89 @@ if __name__ == "__main__":
     #########################
     #### Set experiment CFG.
     #########################
+
     
-    num_of_samples = 3
-    sample_size = 3
-    samples = [random.sample(set(cnn_orig.out_l.values()), k=sample_size) for i in range(num_of_samples)]
     input_range          = (-mnx.large, mnx.large)
     sample_output_range  = (-5        , mnx.large)
     general_output_range = (-mnx.large, mnx.large)
-    out_prop = lambda graph: {n : sample_output_range if n in sample else general_output_range for n in graph.out_l.values()}
-    in_prop  = lambda graph: {n : input_range for n in graph.in_l.values()}
-
-    coi_net_nodes = list()
-    orig_net_nodes = list()
-    coi_net_edges = list()
-    orig_net_edges = list()    
-    coi_timing = list()
-    orig_timing = list()
+    out_prop = lambda graph: {n : sample_output_range if n in sample else general_output_range for n in graph.out_l.values()} #TODO
+    in_prop  = lambda graph: {n : input_range for n in graph.in_l.values()} #TODO
 
     ##############################
     ####Measure Verification Step
     ##############################
-    
-    for j, sample in enumerate(samples):
+
+    coi_nodes   = dict()
+    coi_edges   = dict()
+    coi_runtime  = dict()
+    coi_var = dict()
+    coi_mean = dict()    
+    orig_edges  = list()
+    orig_nodes  = list()
+    orig_runtime = list()   
+
+    N = 10 #Number of samples per k.
+    ks = [1,2,3,4]
+    for k in ks : # number of output neurons.
+
+        samples = [random.sample(set(cnn_orig.out_l.values()), k=k) for i in range(N)]
+
+        coi_nodes[k], coi_edges[k], coi_runtime[k] = list(), list(), list()
+        coi_mean[k], coi_var[k] = list(), list()
+        for j, sample in enumerate(samples):
         
-        print("*********************************** COI **********************************************")    
-        print("COI nodes:{}".format(str(sample)))
-        print("Start solving COI CNN")
-        for v in sample:
-            if v not in cnn_orig.nodes():
-                raise Exception("{} not in cnn_orig".format(v))
-        coi_cnn = mcnn.Cnn2D.coi(cnn_orig, sample)
-        start = time.time()        
-        coi_cnn.solve(in_prop(coi_cnn), out_prop(coi_cnn))
-        end = time.time()
-        coi_timing.append(end - start)
-        coi_net_nodes.append(len(coi_cnn.nodes()))
-        coi_net_edges.append(len(coi_cnn.edges()))
-        print("Finish solving COI CNN")
+            print("*********************************** COI **********************************************")    
+            print("COI nodes:{}".format(str(sample)))
+            print("Start solving COI CNN")
+            for v in sample:
+                if v not in cnn_orig.nodes():
+                    raise Exception("{} not in cnn_orig".format(v))
+                coi_cnn = mcnn.Cnn2D.coi(cnn_orig, sample)
+                start = time.time()        
+                #coi_cnn.solve(in_prop(coi_cnn), out_prop(coi_cnn))
+                end = time.time()
+                coi_runtime[k].append(end - start)
+                coi_nodes[k].append(len(coi_cnn.nodes()))
+                coi_edges[k].append(len(coi_cnn.edges()))
+                cor = [max([int(c) for c in re.findall("%(\d+)", samp)]) for samp in sample]
+                if k > 1:
+                    coi_var[k].append(stat.variance(cor))
+                    coi_mean[k].append(stat.mean(cor))
+                    
+                print("Finish solving COI CNN")
 
     print("*********************************** Original **********************************************")
     print("Start solving CNN")
     start = time.time()        
-    #cnn_orig.solve(in_prop(cnn_orig), out_prop(cnn_orig)) TODO
+    #cnn_orig.solve(in_prop(cnn_orig), out_prop(cnn_orig))
     end = time.time()        
-    orig_timing.append(end - start)
-    orig_net_nodes.append(len(cnn_orig.nodes()))
-    orig_net_edges.append(len(cnn_orig.edges()))    
+    orig_runtime.append(end - start)
+    orig_nodes.append(len(cnn_orig.nodes()))
+    orig_edges.append(len(cnn_orig.edges()))    
     print("Finish solving CNN")
 
     ####################
     ####Process Results
     ####################
             
-    if len(orig_timing) == 1:
-        orig_timing = orig_timing * len(coi_timing)
-    if len(orig_net_nodes) == 1:
-        orig_net_nodes = orig_net_nodes * len(coi_net_nodes)
-    if len(orig_net_edges) == 1:
-        orig_net_edges = orig_net_edges * len(coi_net_edges)
-        
-    coi_mean = stat.mean(coi_timing)    
-    orig_mean = stat.mean(orig_timing)
-    coi_variance = stat.variance(coi_timing)
-    orig_variance = stat.variance(orig_timing)
-    nodes_ratio = [coi/orig for coi, orig in zip(coi_net_nodes, orig_net_nodes)]
-    edges_ratio = [coi/orig for coi, orig in zip(coi_net_edges, orig_net_edges)]
-    time_ratio  = [coi/orig for coi, orig in zip(coi_timing,    orig_timing)]
+    if len(orig_runtime) == 1:
+        orig_runtime = orig_runtime * N
+    if len(orig_nodes) == 1:
+        orig_nodes = orig_nodes * N
+    if len(orig_edges) == 1:
+        orig_edges = orig_edges * N
+
+
+    orig_mean = stat.mean(orig_runtime)    
+    orig_variance = stat.variance(orig_runtime)
+
+
+    coi_rt_mean = {k : stat.mean(coi_runtime[k]) for k in ks}
+    coi_rt_variance = {k : stat.variance(coi_runtime[k]) for k in ks}
+    
+    nodes_ratio = {k : [coi/orig for coi, orig in zip(coi_nodes[k], orig_nodes)] for k in ks} 
+    edges_ratio = {k : [coi/orig for coi, orig in zip(coi_edges[k], orig_edges)] for k in ks}
+    time_ratio  = {k : [coi/orig for coi, orig in zip(coi_runtime[k], orig_runtime)] for k in ks}
 
     ####################
     ####Plot Graphs
@@ -290,17 +304,17 @@ if __name__ == "__main__":
     
     plt.figure(1)
     plt.subplot(131)
-    plt.plot(coi_timing, 'rs', orig_timing, 'b^')
+    plt.plot(coi_runtime[1], 'rs', orig_runtime, 'b^')
     plt.legend(('COI', 'Orig'))
     plt.title('Runtime [seconds]')
     plt.grid(True)
     plt.subplot(132)
-    plt.plot(coi_net_nodes, 'rs', orig_net_nodes, 'b^')
+    plt.plot(coi_nodes[1], 'rs', orig_nodes, 'b^')
     plt.legend(('COI', 'Orig'))    
     plt.title('Nodes')
     plt.grid(True)    
     plt.subplot(133)    
-    plt.plot(coi_net_edges, 'rs', orig_net_edges, 'b^')
+    plt.plot(coi_edges[1], 'rs', orig_edges, 'b^')
     plt.legend(('COI', 'Orig'))    
     plt.title('Edges')
     plt.grid(True)
@@ -308,29 +322,29 @@ if __name__ == "__main__":
     plt.savefig('test_cnn2D_coi_vs_orig_1.png')
 
     plt.figure(2)
-    plt.plot(edges_ratio, 'b', label='Edges')     
-    plt.plot(nodes_ratio, 'y', label='Nodes')    
-    plt.plot(time_ratio, 'g', label='Runtime')
+    plt.plot(edges_ratio[1], 'b', label='Edges')     
+    plt.plot(nodes_ratio[1], 'y', label='Nodes')    
+    plt.plot(time_ratio[1], 'g', label='Runtime')
     plt.title('COI / Orig CNN ratio')
     plt.legend()
     plt.show(block=False)
     plt.savefig('test_cnn2D_coi_vs_orig_2.png')    
 
     plt.figure(3)
-    plt.plot(time_ratio, nodes_ratio, 'r', label='Nodes/Time')
-    plt.plot(time_ratio, edges_ratio, 'b', label='Edges/Time')
+    plt.plot(time_ratio[1], nodes_ratio[1], 'r', label='Nodes/Time')
+    plt.plot(time_ratio[1], edges_ratio[1], 'b', label='Edges/Time')
     plt.title('COI / Orig CNN ratio correlation')
     plt.legend()
     plt.show(block=False)
     plt.savefig('test_cnn2D_coi_vs_orig_3.png')
 
     with open('test_cnn2D_output.txt', 'w') as file:
-        file.write("COI runtime (seconds):{} \n".format(str(coi_timing)))
-        file.write("COI number of nodes:{} \n".format(coi_net_nodes))
-        file.write("COI number of edges:{} \n".format(coi_net_edges))
-        file.write("Orig runtime (seconds):{} \n".format(str(orig_timing)))
-        file.write("Orig number of nodes:{} \n".format(orig_net_nodes))
-        file.write("Orig number of edges:{} \n".format(orig_net_edges))                
+        file.write("COI runtime (seconds):{} \n".format(str(coi_runtime)))
+        file.write("COI number of nodes:{} \n".format(coi_nodes))
+        file.write("COI number of edges:{} \n".format(coi_edges))
+        file.write("Orig runtime (seconds):{} \n".format(str(orig_runtime)))
+        file.write("Orig number of nodes:{} \n".format(orig_nodes))
+        file.write("Orig number of edges:{} \n".format(orig_edges))                
         
     
 
