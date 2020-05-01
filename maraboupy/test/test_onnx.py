2eae4d8bafe262685bea3e1fc29a736e1cd27840
Kyle Julian <kjulian3.stanford.edu>
2020-05-01 21:09:59 +0000

    Add python test option to build

--- maraboupy/test/test_onnx.py
+++ maraboupy/test/test_onnx.py
@@ -5,23 +5,24 @@ warnings.filterwarnings('ignore', category = DeprecationWarning)
 warnings.filterwarnings('ignore', category = PendingDeprecationWarning)
 
 import pytest
-from maraboupy import Marabou
-from maraboupy import MarabouCore
+from .. import Marabou
+from .. import MarabouCore
 import numpy as np
+import os
 
 # Global settings
-OPT = MarabouCore.Options()        # Define options 
-OPT._verbosity = 0                 # Turn off printing
-TOL = 1e-5                         # Set tolerance for checking Marabou evaluations
-NETWORK_FOLDER = "resources/onnx/" # Folder for test networks
+OPT = MarabouCore.Options()              # Define options 
+OPT._verbosity = 0                       # Turn off printing
+TOL = 1e-5                               # Set tolerance for checking Marabou evaluations
+NETWORK_FOLDER = "../../resources/onnx/" # Folder for test networks
 
 def test_fc1():
     """
     Test a simple fully-connected neural network
     Uses Gemm, ReLU, and Identity layers
     """
-    # Load network
-    filename = NETWORK_FOLDER + "fc1.onnx"
+    # Load network relative to this file's location
+    filename = os.path.join(os.path.dirname(__file__), NETWORK_FOLDER, "fc1.onnx")
     network = Marabou.read_onnx(filename)
 
     # Evaluate example point using both Marabou and ONNX
