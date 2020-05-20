fb4ccd1adfa0267cbb4f75996dc4fd3c35665cff
Kyle Julian <kjulian3.stanford.edu>
2020-05-20 04:19:48 +0000

    Updates from tf_upgrade_v2

--- maraboupy/MarabouNetworkTF.py
+++ maraboupy/MarabouNetworkTF.py
@@ -75,24 +75,24 @@ class MarabouNetworkTF(MarabouNetwork.MarabouNetwork):
 
         if savedModel:
             ### Read SavedModel ###
-            sess = tf.Session()
-            tf.saved_model.loader.load(sess, savedModelTags, filename)
+            sess = tf.compat.v1.Session()
+            tf.compat.v1.saved_model.loader.load(sess, savedModelTags, filename)
 
             ### Simplify graph using outputName, which must be specified for SavedModel ###
             simp_graph_def = graph_util.convert_variables_to_constants(sess,sess.graph.as_graph_def(),[outputName])
             with tf.Graph().as_default() as graph:
                 tf.import_graph_def(simp_graph_def, name="")
-            self.sess = tf.Session(graph=graph)
+            self.sess = tf.compat.v1.Session(graph=graph)
             ### End reading SavedModel
 
         else:
             ### Read protobuf file and begin session ###
-            with tf.gfile.GFile(filename, "rb") as f:
-                graph_def = tf.GraphDef()
+            with tf.io.gfile.GFile(filename, "rb") as f:
+                graph_def = tf.compat.v1.GraphDef()
                 graph_def.ParseFromString(f.read())
             with tf.Graph().as_default() as graph:
                 tf.import_graph_def(graph_def, name="")
-            self.sess = tf.Session(graph=graph)
+            self.sess = tf.compat.v1.Session(graph=graph)
             ### END reading protobuf ###
 
         ### Find operations corresponding to input and output ###
