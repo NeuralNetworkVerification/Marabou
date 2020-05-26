a1a3dcc254e7d55dd1ef35dca6848db883b49c59
haozewu <haozewu@stanford.edu>
2020-05-26 16:14:31 -0700

    turn off logging

--- src/configuration/GlobalConfiguration.cpp
+++ src/configuration/GlobalConfiguration.cpp
@@ -84,7 +84,7 @@ const unsigned GlobalConfiguration::RUNTIME_ESTIMATE_THRESHOLD = 5;
 
 // Logging
 const bool GlobalConfiguration::DNC_MANAGER_LOGGING = false;
-const bool GlobalConfiguration::ENGINE_LOGGING = true;
+const bool GlobalConfiguration::ENGINE_LOGGING = false;
 const bool GlobalConfiguration::TABLEAU_LOGGING = false;
 const bool GlobalConfiguration::SMT_CORE_LOGGING = false;
 const bool GlobalConfiguration::DANTZIGS_RULE_LOGGING = false;
