655b515fc3311cece3f33ce9f91ee5bc05e8f7f3
Yuval Jacoby <yuval999@gmail.com>
2020-06-10 11:23:42 +0300

    polish file

--- src/common/MatrixMultiplication.cpp
+++ src/common/MatrixMultiplication.cpp
@@ -1,4 +1,19 @@
-#include<MatrixMultiplication.h>
+/*********************                                                        */
+/*! \file HeapData.cpp
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Guy Katz
+ ** This file is part of the Marabou project.
+ ** Copyright (c) 2017-2019 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved. See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** [[ Add lengthier description here ]]
+
+**/
+
+#include "MatrixMultiplication.h"
 
 #ifdef USE_OPENBLAS 
 #include "cblas.h"
