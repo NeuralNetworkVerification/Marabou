db09b0d30e7b2da2803a2f98074ab4f64301c89c
guyam <guy.amir2@mail.huji.ac.il>
2020-06-25 11:41:31 +0300

    Guy - after system testing

--- src/system_tests/Test_sign.h
+++ src/system_tests/Test_sign.h
@@ -295,10 +295,104 @@ public:
 
 
 
-
-
-
     void test_sign_4() // more advanced BNN
+    {
+        InputQuery inputQuery;
+        inputQuery.setNumberOfVariables( 11 );
+
+        // x0 = 0   -> input to the BNN is negative
+        inputQuery.setLowerBound( 0, -2 );
+        inputQuery.setUpperBound( 0, -2 );
+
+        // means unSAT !
+        inputQuery.setLowerBound( 10, -2.5 ); // unSAT! because x10 = -1 -1 -1 = -3 (always)
+
+        // equations of 1st layer (x0 is input)
+        Equation equation1;
+        equation1.addAddend( 1, 0 );
+        equation1.addAddend( -1, 1 );
+        equation1.setScalar( -2 );
+        inputQuery.addEquation( equation1 ); // x1 = x0 + 2
+
+        Equation equation2;
+        equation2.addAddend( 1, 0 );
+        equation2.addAddend( -1, 2 );
+        equation2.setScalar( 0 );
+        inputQuery.addEquation( equation2 ); // x2 = x0
+
+        Equation equation3;
+        equation3.addAddend( 0, 1 );
+        equation3.addAddend( 1, 3 );
+        equation3.setScalar( 0 );
+        inputQuery.addEquation( equation3 ); // x3 = 0*x1 = 0
+
+
+        //equations of 2nd layer
+        Equation equation4;
+        equation4.addAddend( 1, 1 );
+        equation4.addAddend( 2, 2 );
+        equation4.addAddend( 3, 3 );
+        equation4.addAddend( -1, 4 );
+        equation4.setScalar( 0 );
+        inputQuery.addEquation( equation4 ); // x4 = 1*x1 + 2*x2 +3*x3
+
+
+        Equation equation5;
+        equation5.addAddend( 2, 2 );
+        equation5.addAddend( -1, 5 );
+        equation5.setScalar( 0 );
+        inputQuery.addEquation( equation5 ); // x5 = 2*x2
+
+
+        Equation equation6;
+        equation6.addAddend( 1, 1 );
+        equation6.addAddend( 2, 2 );
+        equation6.addAddend( 3, 3 );
+        equation6.addAddend( -1, 6 );
+        equation6.setScalar( 0 );
+        inputQuery.addEquation( equation6 ); // x6 = 1*x1 + 2*x2 +3*x3
+
+
+
+        //equations of 3rd layer (sign activation layer)
+        SignConstraint *sign1 = new SignConstraint( 4, 7 ); //  x7 = sign (x4)
+        SignConstraint *sign2 = new SignConstraint( 5, 8 ); //  x8 = sign (x5)
+        SignConstraint *sign3 = new SignConstraint( 6, 9 ); //  x9 = sign (x6)
+
+
+        inputQuery.addPiecewiseLinearConstraint( sign1 );
+        inputQuery.addPiecewiseLinearConstraint( sign2 );
+        inputQuery.addPiecewiseLinearConstraint( sign3 );
+
+        //equation of 4th layer (output)
+        Equation equation7;
+        equation7.addAddend( 1, 7 );
+        equation7.addAddend( 1, 8 );
+        equation7.addAddend( 1, 9 );
+        equation7.addAddend( -1, 10 );
+        equation7.setScalar( 0 );
+        inputQuery.addEquation( equation7 ); // x10 = 1*x7 + 1*x8 +1*x9
+
+
+
+        Engine engine;
+        // should return 'false' = unSAT
+
+
+        TS_ASSERT( !engine.processInputQuery( inputQuery ) );
+
+
+//        TS_ASSERT( !engine.solve() );
+
+
+    }
+
+
+
+
+
+
+    void test_sign_5() // more advanced BNN
     {
         InputQuery inputQuery;
         inputQuery.setNumberOfVariables( 11 );
@@ -496,8 +590,216 @@ public:
 
 
         TS_ASSERT( correctSolution );
-        
+
     }
+
+
+
+
+
+
+    void test_sign_6() // more advanced BNN
+    {
+        InputQuery inputQuery;
+        inputQuery.setNumberOfVariables( 11 );
+
+        // -10 <= x0 <= 10   -> input to the BNN is negative
+        inputQuery.setLowerBound( 0, -10 );
+        inputQuery.setUpperBound( 0, 10 );
+
+        // means unSAT !
+        inputQuery.setLowerBound( 10, -3 ); // SAT! because x10 = -1 -1 -1 = -3 (always)
+
+        // equations of 1st layer (x0 is input)
+        Equation equation1;
+        equation1.addAddend( 1, 0 );
+        equation1.addAddend( -1, 1 );
+        equation1.setScalar( -2 );
+        inputQuery.addEquation( equation1 ); // x1 = x0 + 2
+
+        Equation equation2;
+        equation2.addAddend( 1, 0 );
+        equation2.addAddend( -1, 2 );
+        equation2.setScalar( 0 );
+        inputQuery.addEquation( equation2 ); // x2 = x0
+
+        Equation equation3;
+        equation3.addAddend( 0, 1 );
+        equation3.addAddend( 1, 3 );
+        equation3.setScalar( 0 );
+        inputQuery.addEquation( equation3 ); // x3 = 0*x1 = 0
+
+
+        //equations of 2nd layer
+        Equation equation4;
+        equation4.addAddend( 1, 1 );
+        equation4.addAddend( 2, 2 );
+        equation4.addAddend( 3, 3 );
+        equation4.addAddend( -1, 4 );
+        equation4.setScalar( 0 );
+        inputQuery.addEquation( equation4 ); // x4 = 1*x1 + 2*x2 +3*x3
+
+
+        Equation equation5;
+        equation5.addAddend( 2, 2 );
+        equation5.addAddend( -1, 5 );
+        equation5.setScalar( 0 );
+        inputQuery.addEquation( equation5 ); // x5 = 2*x2
+
+
+        Equation equation6;
+        equation6.addAddend( 1, 1 );
+        equation6.addAddend( 2, 2 );
+        equation6.addAddend( 3, 3 );
+        equation6.addAddend( -1, 6 );
+        equation6.setScalar( 0 );
+        inputQuery.addEquation( equation6 ); // x6 = 1*x1 + 2*x2 +3*x3
+
+
+
+        //equations of 3rd layer (sign activation layer)
+        SignConstraint *sign1 = new SignConstraint( 4, 7 ); //  x7 = sign (x4)
+        SignConstraint *sign2 = new SignConstraint( 5, 8 ); //  x8 = sign (x5)
+        SignConstraint *sign3 = new SignConstraint( 6, 9 ); //  x9 = sign (x6)
+
+
+        inputQuery.addPiecewiseLinearConstraint( sign1 );
+        inputQuery.addPiecewiseLinearConstraint( sign2 );
+        inputQuery.addPiecewiseLinearConstraint( sign3 );
+
+        //equation of 4th layer (output)
+        Equation equation7;
+        equation7.addAddend( 1, 7 );
+        equation7.addAddend( 1, 8 );
+        equation7.addAddend( 1, 9 );
+        equation7.addAddend( -1, 10 );
+        equation7.setScalar( 0 );
+        inputQuery.addEquation( equation7 ); // x10 = 1*x7 + 1*x8 +1*x9
+
+
+
+        Engine engine;
+        // should return SAT
+
+
+        TS_ASSERT(engine.processInputQuery( inputQuery ) );
+
+        TS_ASSERT( engine.solve() );
+
+
+
+
+        engine.extractSolution( inputQuery );
+
+        bool correctSolution = true;
+        // Sanity test
+
+
+        double value_x0 = inputQuery.getSolutionValue( 0 );
+        double value_x1 = inputQuery.getSolutionValue( 1 );
+        double value_x2 = inputQuery.getSolutionValue( 2 );
+        double value_x3 = inputQuery.getSolutionValue( 3 );
+        double value_x4 = inputQuery.getSolutionValue( 4 );
+        double value_x5 = inputQuery.getSolutionValue( 5 );
+        double value_x6 = inputQuery.getSolutionValue( 6 );
+        double value_x7 = inputQuery.getSolutionValue( 7 );
+        double value_x8 = inputQuery.getSolutionValue( 8 );
+        double value_x9 = inputQuery.getSolutionValue( 9 );
+        double value_x10 = inputQuery.getSolutionValue( 10 );
+
+
+        // check first layer variables
+        if ( !FloatUtils::areEqual( value_x1, value_x0 + 2 ) ) // we want x1 = x0 + 2
+            correctSolution = false;
+
+
+        if ( !FloatUtils::areEqual( value_x2, value_x0 ) ) // we want x2 = x0
+            correctSolution = false;
+
+
+        if ( !FloatUtils::areEqual( value_x3, 0 ) ) // we want x3 = 0
+            correctSolution = false;
+
+
+
+        // check 2nd layer variables
+        if ( !FloatUtils::areEqual( value_x4, value_x1 + 2*value_x2 + 3*value_x3 ) )
+            // we want x4 = 1*x1 + 2*x2 + 3*x3
+            correctSolution = false;
+
+
+        if ( !FloatUtils::areEqual( value_x5, 2*value_x2 ) )
+            // we want x5 = 2*x2
+            correctSolution = false;
+
+
+        if ( !FloatUtils::areEqual( value_x6, value_x1 + 2*value_x2 + 3*value_x3 ) )
+            // we want x6 = 1*x1 + 2*x2 + 3*x3
+            correctSolution = false;
+
+
+
+        // check 3rd layer
+
+        // we want x7 = sign(x4)
+        if (!FloatUtils::lt(value_x4, 0)) // if x4>= 0 -> we want x7 = 1
+        {
+            if (!FloatUtils::areEqual( value_x7, 1 )) {
+                correctSolution = false;
+            }
+        }
+
+        if (FloatUtils::lt(value_x4, 0))// if x4< 0 -> we want x7 = - 1
+        {
+            if (!FloatUtils::areEqual( value_x7, -1 )) {
+                correctSolution = false;
+            }
+        }
+
+        // we want x8 = sign(x5)
+        if (!FloatUtils::lt(value_x5, 0)) // if x5>= 0 -> we want x8 = 1
+        {
+            if (!FloatUtils::areEqual( value_x8, 1 )) {
+                correctSolution = false;
+            }
+        }
+
+        if (FloatUtils::lt(value_x5, 0))// if x5< 0 -> we want x8 = - 1
+        {
+            if (!FloatUtils::areEqual( value_x8, -1 )) {
+                correctSolution = false;
+            }
+        }
+
+
+
+        // we want x9 = sign(x6)
+        if (!FloatUtils::lt(value_x6, 0)) // if x6>= 0 -> we want x9 = 1
+        {
+            if (!FloatUtils::areEqual( value_x9, 1 )) {
+                correctSolution = false;
+            }
+        }
+
+        if (FloatUtils::lt(value_x6, 0))// if x6< 0 -> we want x9 = - 1
+        {
+            if (!FloatUtils::areEqual( value_x9, -1 )) {
+                correctSolution = false;
+            }
+        }
+
+
+        // check last layer variable (BNN output)
+        if ( !FloatUtils::areEqual( value_x10, value_x7 + value_x8 + value_x9 ) )
+            // we want x10 = x7 + x8 +x9
+            correctSolution = false;
+
+
+        TS_ASSERT( correctSolution );
+
+    }
+
+
 
 
 
