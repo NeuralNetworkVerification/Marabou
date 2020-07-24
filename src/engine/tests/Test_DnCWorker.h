0434c8bd613e52b19e3c7b50a0b99a94c6d36bb0
haozewu <haozewu@stanford.edu>
2020-07-24 16:47:31 -0700

    separate tests

--- src/engine/tests/Test_DnCWorker.h
+++ src/engine/tests/Test_DnCWorker.h
@@ -71,37 +71,7 @@ public:
         return counter;
     }
 
-    void createPlaceHolderSubQuery()
-    {
-        // Add a subQuery to workload.
-        // This subQuery serves only as a placeholder, as the exitCode of the
-        // mock engine after solving this subQuery does not depend on it.
-
-        SubQuery *subQuery = new SubQuery;
-
-        auto split = std::unique_ptr<PiecewiseLinearCaseSplit>
-            ( new PiecewiseLinearCaseSplit );
-        Tightening bound1( 1, -2.0, Tightening::LB );
-        Tightening bound2( 1, 2.0, Tightening::UB );
-        Tightening bound3( 2, 3.0, Tightening::LB );
-        Tightening bound4( 2, 5.0, Tightening::UB );
-        Tightening bound5( 3, 2.0, Tightening::LB );
-        Tightening bound6( 3, 5.0, Tightening::UB );
-        split->storeBoundTightening( bound1 );
-        split->storeBoundTightening( bound2 );
-        split->storeBoundTightening( bound3 );
-        split->storeBoundTightening( bound4 );
-        split->storeBoundTightening( bound5 );
-        split->storeBoundTightening( bound6 );
-
-        subQuery->_queryId = "";
-        subQuery->_split = std::move( split );
-        subQuery->_timeoutInSeconds = 5;
-        TS_ASSERT( _workload->push( std::move( subQuery ) ) );
-    }
-
-    // Test different branches of DnCWorker.popOneSubQueryAndSolve()
-    void test_pop_one_sub_query_and_solve()
+    void test_case_timeout()
     {
         //  Pop a subQuery from the workload, set the mock engine to report
         //  timeout on solving the subQuery.
@@ -132,7 +102,10 @@ public:
         TS_ASSERT( clearSubQueries() == 4 );
         TS_ASSERT( numUnsolvedSubQueries.load() == 4 );
         TS_ASSERT( !shouldQuitSolving.load() );
+    }
 
+    void test_case_unsat_not_done()
+    {
         //  Pop 2 subQueries from the workload, set the mock engine to report
         //  unsat on solving it.
         //  In this case, we only decrement the value of numUnsolvedSubQueries
@@ -140,23 +113,32 @@ public:
         //  1. The size of workload should become 1
         //  2. The value of numUnsolvedSubQueries should become 1
         //  3. The value of shouldQuitSolving should still be false
+
+        // Create the subQuery to be added to workLoad
         TS_ASSERT( clearSubQueries() == 0 );
 
         createPlaceHolderSubQuery();
         createPlaceHolderSubQuery();
         _engine->setExitCode( IEngine::UNSAT );
-        numUnsolvedSubQueries= 2;
-        shouldQuitSolving= false;
-		dncWorker = DnCWorker( _workload, _engine, numUnsolvedSubQueries,
-                               shouldQuitSolving, threadId, onlineDivides,
-                               timeoutFactor, divideStrategy );
+        std::atomic_uint numUnsolvedSubQueries( 2 );
+        std::atomic_bool shouldQuitSolving( false );
+        unsigned threadId = 0;
+        unsigned onlineDivides = 2;
+        float timeoutFactor = 1;
+        DivideStrategy divideStrategy = DivideStrategy::LargestInterval;
+        DnCWorker dncWorker( _workload, _engine, numUnsolvedSubQueries,
+                             shouldQuitSolving, threadId, onlineDivides,
+                              timeoutFactor, divideStrategy );
 
         dncWorker.popOneSubQueryAndSolve();
         TS_ASSERT( _engine->getExitCode() == IEngine::UNSAT );
         TS_ASSERT( clearSubQueries() == 1 );
         TS_ASSERT( numUnsolvedSubQueries.load() == 1 );
         TS_ASSERT( !shouldQuitSolving.load() );
+    }
 
+    void test_case_unsat_done()
+    {
         //  Pop a subQuery from the workload, set the mock engine to report
         //  unsat on solving it.
         //  In this case, we only decrement the value of numUnsolvedSubQueries
@@ -170,19 +152,25 @@ public:
 
         createPlaceHolderSubQuery();
         _engine->setExitCode( IEngine::UNSAT );
-        numUnsolvedSubQueries= 1;
-        shouldQuitSolving= false;
-
-        dncWorker = DnCWorker( _workload, _engine, numUnsolvedSubQueries,
-                               shouldQuitSolving, threadId, onlineDivides,
-                               timeoutFactor, divideStrategy );
+        std::atomic_uint numUnsolvedSubQueries( 1 );
+        std::atomic_bool shouldQuitSolving( false );
+        unsigned threadId = 0;
+        unsigned onlineDivides = 2;
+        float timeoutFactor = 1;
+        DivideStrategy divideStrategy = DivideStrategy::LargestInterval;
+        DnCWorker dncWorker( _workload, _engine, numUnsolvedSubQueries,
+                             shouldQuitSolving, threadId, onlineDivides,
+                              timeoutFactor, divideStrategy );
 
         dncWorker.popOneSubQueryAndSolve();
         TS_ASSERT( _engine->getExitCode() == IEngine::UNSAT );
         TS_ASSERT( clearSubQueries() == 0 );
         TS_ASSERT( numUnsolvedSubQueries.load() == 0 );
         TS_ASSERT( shouldQuitSolving.load() );
+    }
 
+    void test_case_sat()
+    {
         //  Pop a subQuery from the workload, set the mock engine to report
         //  sat on solving it.
         //  In this case, we decrement the value of numUnsolvedSubQueries, and
@@ -191,13 +179,19 @@ public:
         //  1. The size of workload should become 0
         //  2. The value of numUnsolvedSubQueries should become 0
         //  3. The value of shouldQuitSolving should become true
+
+        // Create the subQuery to be added to workLoad
         TS_ASSERT( clearSubQueries() == 0 );
 
         createPlaceHolderSubQuery();
         _engine->setExitCode( IEngine::SAT );
-        numUnsolvedSubQueries=( 1 );
-        shouldQuitSolving=( false );
-		dncWorker = DnCWorker( _workload, _engine, numUnsolvedSubQueries,
+        std::atomic_uint numUnsolvedSubQueries( 1 );
+        std::atomic_bool shouldQuitSolving( false );
+        unsigned threadId = 0;
+        unsigned onlineDivides = 2;
+        float timeoutFactor = 1;
+        DivideStrategy divideStrategy = DivideStrategy::LargestInterval;
+        DnCWorker dncWorker( _workload, _engine, numUnsolvedSubQueries,
                              shouldQuitSolving, threadId, onlineDivides,
                               timeoutFactor, divideStrategy );
 
@@ -206,7 +200,10 @@ public:
         TS_ASSERT( clearSubQueries() == 0 );
         TS_ASSERT( numUnsolvedSubQueries.load() == 0 );
         TS_ASSERT( shouldQuitSolving.load() );
+    }
 
+    void test_case_quit_requested()
+    {
         //  Pop a subQuery from the workload, set the mock engine's exitCode
         //  to be QuitRequested, and shouldQuitSolving to be true.
         //  In this case, the DnCWorker should quit.
@@ -218,15 +215,22 @@ public:
 
         createPlaceHolderSubQuery();
         _engine->setExitCode( IEngine::QUIT_REQUESTED );
-        numUnsolvedSubQueries= 1;
-        shouldQuitSolving =  true;
-		dncWorker = DnCWorker( _workload, _engine, numUnsolvedSubQueries,
-                               shouldQuitSolving, threadId, onlineDivides,
-                               timeoutFactor, divideStrategy );
+        std::atomic_uint numUnsolvedSubQueries( 1 );
+        std::atomic_bool shouldQuitSolving( true );
+        unsigned threadId = 0;
+        unsigned onlineDivides = 2;
+        float timeoutFactor = 1;
+        DivideStrategy divideStrategy = DivideStrategy::LargestInterval;
+        DnCWorker dncWorker( _workload, _engine, numUnsolvedSubQueries,
+                             shouldQuitSolving, threadId, onlineDivides,
+                              timeoutFactor, divideStrategy );
         dncWorker.popOneSubQueryAndSolve();
         TS_ASSERT( _engine->getExitCode() == IEngine::QUIT_REQUESTED );
         TS_ASSERT( numUnsolvedSubQueries.load() == 1 );
+    }
 
+    void test_case_error()
+    {
         //  Pop a subQuery from the workload, set the mock engine's exitCode
         //  to be error
         //  In this case, the DnCWorker should quit.
@@ -239,16 +243,49 @@ public:
 
         createPlaceHolderSubQuery();
         _engine->setExitCode( IEngine::ERROR );
-         numUnsolvedSubQueries= 1;
-        shouldQuitSolving = false;
-		dncWorker = DnCWorker( _workload, _engine, numUnsolvedSubQueries,
-                               shouldQuitSolving, threadId, onlineDivides,
-                               timeoutFactor, divideStrategy );
+        std::atomic_uint numUnsolvedSubQueries( 1 );
+        std::atomic_bool shouldQuitSolving( false );
+        unsigned threadId = 0;
+        unsigned onlineDivides = 2;
+        float timeoutFactor = 1;
+        DivideStrategy divideStrategy = DivideStrategy::LargestInterval;
+        DnCWorker dncWorker( _workload, _engine, numUnsolvedSubQueries,
+                             shouldQuitSolving, threadId, onlineDivides,
+                              timeoutFactor, divideStrategy );
 
         dncWorker.popOneSubQueryAndSolve();
         TS_ASSERT( _engine->getExitCode() == IEngine::ERROR );
         TS_ASSERT( numUnsolvedSubQueries.load() == 1 );
         TS_ASSERT( shouldQuitSolving.load() );
+    }
+
+    void createPlaceHolderSubQuery()
+    {
+        // Add a subQuery to workload.
+        // This subQuery serves only as a placeholder, as the exitCode of the
+        // mock engine after solving this subQuery does not depend on it.
+
+        SubQuery *subQuery = new SubQuery;
+
+        auto split = std::unique_ptr<PiecewiseLinearCaseSplit>
+            ( new PiecewiseLinearCaseSplit );
+        Tightening bound1( 1, -2.0, Tightening::LB );
+        Tightening bound2( 1, 2.0, Tightening::UB );
+        Tightening bound3( 2, 3.0, Tightening::LB );
+        Tightening bound4( 2, 5.0, Tightening::UB );
+        Tightening bound5( 3, 2.0, Tightening::LB );
+        Tightening bound6( 3, 5.0, Tightening::UB );
+        split->storeBoundTightening( bound1 );
+        split->storeBoundTightening( bound2 );
+        split->storeBoundTightening( bound3 );
+        split->storeBoundTightening( bound4 );
+        split->storeBoundTightening( bound5 );
+        split->storeBoundTightening( bound6 );
+
+        subQuery->_queryId = "";
+        subQuery->_split = std::move( split );
+        subQuery->_timeoutInSeconds = 5;
+        TS_ASSERT( _workload->push( std::move( subQuery ) ) );
     }
 };
 
