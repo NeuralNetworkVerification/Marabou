0f22e5bf18923a5fdcb8666ba9e514157e0237c3
guyam <guy.amir2@mail.huji.ac.il>
2020-08-11 17:03:38 +0300

    with sign function - after Guy A & Kyle

--- maraboupy/MarabouNetwork.py
+++ maraboupy/MarabouNetwork.py
@@ -10,7 +10,6 @@ Copyright (c) 2017-2019 by the authors listed in the file AUTHORS
 in the top-level source directory) and their institutional affiliations.
 All rights reserved. See the file COPYING in the top-level source
 directory for licensing information.
-
 MarabouNetwork defines an abstract class that represents neural networks with piecewise linear constraints
 '''
 
@@ -21,13 +20,14 @@ import numpy as np
 
 class MarabouNetwork:
     """Abstract class representing general Marabou network
-    
+
     Attributes:
         numVars (int): Total number of variables to represent network
         equList (list of :class:`~maraboupy.MarabouUtils.Equation`): Network equations
         reluList (list of tuples): List of relu constraint tuples, where each tuple contains the backward and forward variables
         maxList (list of tuples): List of max constraint tuples, where each tuple conatins the set of input variables and output variable
         absList (list of tuples): List of abs constraint tuples, where each tuple conatins the input variable and the output variable
+        signList (list of tuples): List of sign constraint tuples, where each tuple conatins the input variable and the output variable
         varsParticipatingInConstraints (set of int): Variables involved in some constraint
         lowerBounds (Dict[int, float]): Lower bounds of variables
         upperBounds (Dict[int, float]): Upper bounds of variables
@@ -47,6 +47,7 @@ class MarabouNetwork:
         self.reluList = []
         self.maxList = []
         self.absList = []
+        self.signList = []
         self.varsParticipatingInConstraints = set()
         self.lowerBounds = dict()
         self.upperBounds = dict()
@@ -55,10 +56,8 @@ class MarabouNetwork:
 
     def getNewVariable(self):
         """Function to create a new variable
-
         Returns:
             (int): New variable number
-
         :meta private:
         """
         self.numVars += 1
@@ -66,7 +65,6 @@ class MarabouNetwork:
 
     def addEquation(self, x):
         """Function to add new equation to the network
-
         Args:
             x (:class:`~maraboupy.MarabouUtils.Equation`): New equation to add
         """
@@ -74,7 +72,6 @@ class MarabouNetwork:
 
     def setLowerBound(self, x, v):
         """Function to set lower bound for variable
-
         Args:
             x (int): Variable number to set
             v (float): Value representing lower bound
@@ -83,7 +80,6 @@ class MarabouNetwork:
 
     def setUpperBound(self, x, v):
         """Function to set upper bound for variable
-
         Args:
             x (int): Variable number to set
             v (float): Value representing upper bound
@@ -92,7 +88,6 @@ class MarabouNetwork:
 
     def addRelu(self, v1, v2):
         """Function to add a new Relu constraint
-
         Args:
             v1 (int): Variable representing input of Relu
             v2 (int): Variable representing output of Relu
@@ -103,7 +98,6 @@ class MarabouNetwork:
 
     def addMaxConstraint(self, elements, v):
         """Function to add a new Max constraint
-
         Args:
             elements (set of int): Variable representing input to max constraint
             v (int): Variable representing output of max constraint
@@ -115,7 +109,6 @@ class MarabouNetwork:
 
     def addAbsConstraint(self, b, f):
         """Function to add a new Abs constraint
-
         Args:
             b (int): Variable representing input of the Abs constraint
             f (int): Variable representing output of the Abs constraint
@@ -124,9 +117,18 @@ class MarabouNetwork:
         self.varsParticipatingInConstraints.add(b)
         self.varsParticipatingInConstraints.add(f)
 
+    def addSignConstraint(self, b, f):
+        """Function to add a new Sign constraint
+        Args:
+            b (int): Variable representing input of Sign
+            f (int): Variable representing output of Sign
+        """
+        self.signList += [(b, f)]
+        self.varsParticipatingInConstraints.add(b)
+        self.varsParticipatingInConstraints.add(f)
+
     def lowerBoundExists(self, x):
         """Function to check whether lower bound for a variable is known
-
         Args:
             x (int): Variable to check
         """
@@ -134,7 +136,6 @@ class MarabouNetwork:
 
     def upperBoundExists(self, x):
         """Function to check whether upper bound for a variable is known
-
         Args:
             x (int): Variable to check
         """
@@ -142,7 +143,6 @@ class MarabouNetwork:
 
     def participatesInPLConstraint(self, x):
         """Function to check whether variable participates in any piecewise linear constraint in this network
-
         Args:
             x (int): Variable to check
         """
@@ -151,10 +151,8 @@ class MarabouNetwork:
 
     def addEquality(self, vars, coeffs, scalar):
         """Function to add equality constraint to network
-
         .. math::
             \sum_i vars_i * coeffs_i = scalar
-
         Args:
             vars (list of int): Variable numbers
             coeffs (list of float): Coefficients
@@ -169,10 +167,8 @@ class MarabouNetwork:
 
     def addInequality(self, vars, coeffs, scalar):
         """Function to add inequality constraint to network
-
         .. math::
             \sum_i vars_i * coeffs_i \le scalar
-
         Args:
             vars (list of int): Variable numbers
             coeffs (list of float): Coefficients
@@ -187,7 +183,6 @@ class MarabouNetwork:
 
     def getMarabouQuery(self):
         """Function to convert network into Marabou InputQuery
-
         Returns:
             :class:`~maraboupy.MarabouCore.InputQuery`
         """
@@ -226,6 +221,9 @@ class MarabouNetwork:
         for b, f in self.absList:
             MarabouCore.addAbsConstraint(ipq, b, f)
 
+        for b, f in self.signList:
+            MarabouCore.addSignConstraint(ipq, b, f)
+
         for l in self.lowerBounds:
             assert l < self.numVars
             ipq.setLowerBound(l, self.lowerBounds[l])
@@ -233,17 +231,15 @@ class MarabouNetwork:
         for u in self.upperBounds:
             assert u < self.numVars
             ipq.setUpperBound(u, self.upperBounds[u])
-            
+
         return ipq
 
     def solve(self, filename="", verbose=True, options=None):
         """Function to solve query represented by this network
-
         Args:
             filename (string): Path for redirecting output
             verbose (bool): If true, print out solution after solve finishes
             options (:class:`~maraboupy.MarabouCore.Options`): Object for specifying Marabou options, defaults to None
-
         Returns:
             (tuple): tuple containing:
                 - vals (Dict[int, float]): Empty dictionary if UNSAT, otherwise a dictionary of SATisfying values for variables
@@ -271,7 +267,6 @@ class MarabouNetwork:
 
     def saveQuery(self, filename=""):
         """Serializes the inputQuery in the given filename
-
         Args:
             filename: (string) file to write serialized inputQuery
         """
@@ -280,18 +275,16 @@ class MarabouNetwork:
 
     def evaluateWithMarabou(self, inputValues, filename="evaluateWithMarabou.log", options=None):
         """Function to evaluate network at a given point using Marabou as solver
-
         Args:
             inputValues (list of np arrays): Inputs to evaluate
             filename (str): Path to redirect output if using Marabou solver, defaults to "evaluateWithMarabou.log"
             options (:class:`~maraboupy.MarabouCore.Options`): Object for specifying Marabou options, defaults to None
-
         Returns:
             (np array): Values representing the output of the network or None if system is UNSAT
         """
         # Make sure inputValues is a list of np arrays and not list of lists
         inputValues = [np.array(inVal) for inVal in inputValues]
-        
+
         inputVars = self.inputVars # list of numpy arrays
         outputVars = self.outputVars
 
@@ -323,13 +316,11 @@ class MarabouNetwork:
 
     def evaluate(self, inputValues, useMarabou=True, options=None, filename="evaluateWithMarabou.log"):
         """Function to evaluate network at a given point
-
         Args:
             inputValues (list of np arrays): Inputs to evaluate
             useMarabou (bool): Whether to use Marabou solver or TF/ONNX, defaults to True
             options (:class:`~maraboupy.MarabouCore.Options`): Object for specifying Marabou options, defaults to None
             filename (str): Path to redirect output if using Marabou solver, defaults to "evaluateWithMarabou.log"
-
         Returns:
             (np array): Values representing the output of the network or None if output cannot be computed
         """
@@ -340,12 +331,10 @@ class MarabouNetwork:
 
     def findError(self, inputValues, options=None, filename="evaluateWithMarabou.log"):
         """Function to find error between Marabou solver and TF/Nnet at a given point
-
         Args:
             inputValues (list of np arrays): Input values to evaluate
             options (:class:`~maraboupy.MarabouCore.Options`) Object for specifying Marabou options, defaults to None
             filename (str): Path to redirect output if using Marabou solver, defaults to "evaluateWithMarabou.log"
-
         Returns:
             (np array): Values representing the error in each output variable
         """
@@ -353,4 +342,3 @@ class MarabouNetwork:
         outNotMar = self.evaluate(inputValues, useMarabou=False, options=options, filename=filename)
         err = np.abs(outMar - outNotMar)
         return err
-
