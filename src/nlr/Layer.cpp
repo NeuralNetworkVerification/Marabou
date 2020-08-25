988ad7821f9e1b3a0e3ef49ac2f8707309a8c114
Guy Katz <guyk@cs.stanford.edu>
2020-08-25 10:09:30 +0300

    Optimiziations for Sign constraints (#356)
    * parse sign layers in the NLR
    
    * LP encoding for Signs
    
    * interval arithmetic propagation for sign
    
    * bug fix
    
    * default, trivial SBT, for activations that are not yet supported
    
    * topological order, and some printing
    
    Co-authored-by: Guy Katz <guykatz@cs.huji.ac.il>

--- src/nlr/Layer.cpp
+++ src/nlr/Layer.cpp
@@ -168,6 +168,17 @@ void Layer::computeAssignment()
         }
     }
 
+    else if ( _type == SIGN )
+    {
+        for ( unsigned i = 0; i < _size; ++i )
+        {
+            NeuronIndex sourceIndex = *_neuronToActivationSources[i].begin();
+            double inputValue = _layerOwner->getLayer( sourceIndex._layer )->getAssignment( sourceIndex._neuron );
+
+            _assignment[i] = FloatUtils::isNegative( inputValue ) ? -1 : 1;
+        }
+    }
+
     else
     {
         printf( "Error! Neuron type %u unsupported\n", _type );
@@ -244,7 +255,7 @@ double Layer::getBias( unsigned neuron ) const
 
 void Layer::addActivationSource( unsigned sourceLayer, unsigned sourceNeuron, unsigned targetNeuron )
 {
-    ASSERT( _type == RELU || _type == ABSOLUTE_VALUE || _type == MAX );
+    ASSERT( _type == RELU || _type == ABSOLUTE_VALUE || _type == MAX || _type == SIGN );
 
     if ( !_neuronToActivationSources.exists( targetNeuron ) )
         _neuronToActivationSources[targetNeuron] = List<NeuronIndex>();
@@ -252,7 +263,7 @@ void Layer::addActivationSource( unsigned sourceLayer, unsigned sourceNeuron, un
     _neuronToActivationSources[targetNeuron].append( NeuronIndex( sourceLayer, sourceNeuron ) );
 
     DEBUG({
-            if ( _type == RELU || _type == ABSOLUTE_VALUE )
+            if ( _type == RELU || _type == ABSOLUTE_VALUE || _type == SIGN )
                 ASSERT( _neuronToActivationSources[targetNeuron].size() == 1 );
         });
 }
@@ -345,10 +356,14 @@ void Layer::computeIntervalArithmeticBounds()
         computeIntervalArithmeticBoundsForAbs();
         break;
 
+    case SIGN:
+        computeIntervalArithmeticBoundsForSign();
+        break;
+
     case MAX:
 
     default:
-        printf( "Error! Actiation type %u unsupported\n", _type );
+        printf( "Error! Activation type %u unsupported\n", _type );
         throw MarabouError( MarabouError::NETWORK_LEVEL_REASONER_ACTIVATION_NOT_SUPPORTED );
         break;
     }
@@ -501,6 +516,37 @@ void Layer::computeIntervalArithmeticBoundsForAbs()
     }
 }
 
+void Layer::computeIntervalArithmeticBoundsForSign()
+{
+    for ( unsigned i = 0; i < _size; ++i )
+    {
+        if ( _eliminatedNeurons.exists( i ) )
+            continue;
+
+        NeuronIndex sourceIndex = *_neuronToActivationSources[i].begin();
+        const Layer *sourceLayer = _layerOwner->getLayer( sourceIndex._layer );
+
+        double lb = sourceLayer->getLb( sourceIndex._neuron );
+        double ub = sourceLayer->getUb( sourceIndex._neuron );
+
+        if ( !FloatUtils::isNegative( lb ) )
+        {
+            _lb[i] = 1;
+            _ub[i] = 1;
+        }
+        else if ( FloatUtils::isNegative( ub ) )
+        {
+            _lb[i] = -1;
+            _ub[i] = -1;
+        }
+        else
+        {
+            _lb[i] = -1;
+            _ub[i] = 1;
+        }
+    }
+}
+
 void Layer::computeSymbolicBounds()
 {
     switch ( _type )
@@ -522,12 +568,44 @@ void Layer::computeSymbolicBounds()
         computeSymbolicBoundsForAbsoluteValue();
         break;
 
-    case MAX:
-
     default:
-        printf( "Error! Actiation type %u unsupported\n", _type );
-        throw MarabouError( MarabouError::NETWORK_LEVEL_REASONER_ACTIVATION_NOT_SUPPORTED );
+        computeSymbolicBoundsDefault();
         break;
+    }
+}
+
+void Layer::computeSymbolicBoundsDefault()
+{
+    // This is the default operation, for layers that are not
+    // supported yet. The "symbolic" bounds computed are just the
+    // concrete bounds.
+
+    std::fill_n( _symbolicLb, _size * _inputLayerSize, 0 );
+    std::fill_n( _symbolicUb, _size * _inputLayerSize, 0 );
+
+    for ( unsigned i = 0; i < _size; ++i )
+    {
+        double lb;
+        double ub;
+
+        if ( _eliminatedNeurons.exists( i ) )
+        {
+            lb = _eliminatedNeurons[i];
+            ub = _eliminatedNeurons[i];
+        }
+        else
+        {
+            lb = _lb[i];
+            ub = _ub[i];
+        }
+
+        _symbolicLowerBias[i] = lb;
+        _symbolicUpperBias[i] = ub;
+
+        _symbolicLbOfLb[i] = lb;
+        _symbolicUbOfLb[i] = ub;
+        _symbolicLbOfUb[i] = lb;
+        _symbolicUbOfUb[i] = ub;
     }
 }
 
@@ -1281,6 +1359,10 @@ String Layer::typeToString( Type type )
         return "MAX";
         break;
 
+    case SIGN:
+        return "SIGN";
+        break;
+
     default:
         return "UNKNOWN TYPE";
         break;
@@ -1337,6 +1419,7 @@ void Layer::dump() const
     case RELU:
     case ABSOLUTE_VALUE:
     case MAX:
+    case SIGN:
 
         for ( unsigned i = 0; i < _size; ++i )
         {
