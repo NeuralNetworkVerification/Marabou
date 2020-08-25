1480c7180fd226a1d0ce9870cd49715464689bab
guyam <guy.amir2@mail.huji.ac.il>
2020-08-25 10:29:08 +0300

    Merge branch 'master' of https://github.com/NeuralNetworkVerification/Marabou
    # Conflicts:
    #	src/engine/InputQuery.cpp
    #	src/engine/InputQuery.h

--- src/configuration/GlobalConfiguration.cpp
+++ src/configuration/GlobalConfiguration.cpp
@@ -51,7 +51,7 @@ const double GlobalConfiguration::COST_FUNCTION_ERROR_THRESHOLD = 0.0000000001;
 
 const bool GlobalConfiguration::USE_HARRIS_RATIO_TEST = true;
 
-const bool GlobalConfiguration::USE_SYMBOLIC_BOUND_TIGHTENING = true;
+const bool GlobalConfiguration::USE_SYMBOLIC_BOUND_TIGHTENING = false;
 const double GlobalConfiguration::SYMBOLIC_TIGHTENING_ROUNDING_CONSTANT = 0.00000005;
 
 const bool GlobalConfiguration::PREPROCESS_INPUT_QUERY = true;
