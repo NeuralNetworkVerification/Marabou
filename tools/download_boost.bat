da16a2e050d0d2eeca9090c26c817268dee605dc
anwu1219 <haozewu@stanford.edu>
2020-09-08 09:43:13 -0700

    add date time

--- tools/download_boost.bat
+++ tools/download_boost.bat
@@ -13,6 +13,6 @@ ECHO Installing Boost
 CD boost_1_68_0
 
 CALL bootstrap.bat >NUL
-CALL b2.exe --prefix="%cd%\win_installed" --with-program_options --with-timer --with-chrono --with-thread --with-atomic link=static install toolset=msvc >NUL
+CALL b2.exe --prefix="%cd%\win_installed" --with-program_options --with-timer --with-chrono --with-thread --with-atomic --with-date_time link=static install toolset=msvc >NUL
 
 CD %curdir%
