28258a88fc8064deca5a57796da8370b65d7d109
anwu1219 <haozewu@stanford.edu>
2020-09-29 14:49:56 -0700

    polarity in sign constriant

--- src/engine/SignConstraint.h
+++ src/engine/SignConstraint.h
@@ -150,9 +150,40 @@ public:
     unsigned getB() const;
     unsigned getF() const;
 
+  bool supportPolarity() const;
+
+  /*
+    Return the polarity of the Sign Constraint, which computes how symmetric
+    the bound of the input to this ReLU is with respect to 0.
+    Let LB be the lowerbound, and UB be the upperbound.
+    If LB >= 0, polarity is 1.
+    If UB <= 0, polarity is -1.
+    If LB < 0, and UB > 0, polarity is ( LB + UB ) / (UB - LB).
+
+    We divide the sum by the width of the interval so that the polarity is
+    always between -1 and 1. The closer it is to 0, the more symmetric the
+    bound is.
+  */
+  double computePolarity() const;
+
+  /*
+    Update the preferred direction for fixing and handling case split
+  */
+  void updateDirection();
+
+  PhaseStatus getDirection() const;
+
+  void updateScoreBasedOnPolarity();
+
 private:
     unsigned _b, _f;
     PhaseStatus _phaseStatus;
+
+    /*
+      Denotes which case split to handle first.
+      And which phase status to repair a relu into.
+    */
+    PhaseStatus _direction;
 
     PiecewiseLinearCaseSplit getNegativeSplit() const;
     PiecewiseLinearCaseSplit getPositiveSplit() const;
