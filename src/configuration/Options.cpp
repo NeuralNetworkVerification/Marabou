5c71a51f13a6f7919e6406f16b1b1b9d1d949f17
anwu1219 <haozewu@stanford.edu>
2020-10-01 09:12:09 -0700

    fix

--- src/configuration/Options.cpp
+++ src/configuration/Options.cpp
@@ -70,6 +70,7 @@ void Options::initializeDefaultValues()
     _stringOptions[PROPERTY_FILE_PATH] = "";
     _stringOptions[INPUT_QUERY_FILE_PATH] = "";
     _stringOptions[SUMMARY_FILE] = "";
+    _stringOptions[SPLITTING_STRATEGY] = "";
     _stringOptions[SNC_SPLITTING_STRATEGY] = "";
     _stringOptions[QUERY_DUMP_FILE] = "";
 }
