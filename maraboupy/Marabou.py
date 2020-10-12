33b0805de84b102043e45ef84bad43aa3d1f5ffb
anwu1219 <haozewu@stanford.edu>
2020-10-12 11:38:54 -0700

    add splitTreshold to options

--- maraboupy/Marabou.py
+++ maraboupy/Marabou.py
@@ -120,7 +120,7 @@ def solve_query(ipq, filename="", verbose=True, options=None):
 def createOptions(numWorkers=1, initialTimeout=5, initialDivides=0, onlineDivides=2,
                   timeoutInSeconds=0, timeoutFactor=1.5, verbosity=2, snc=False,
                   splittingStrategy="auto", sncSplittingStrategy="auto",
-                  restoreTreeStates=False ):
+                  restoreTreeStates=False, splitThreshold=20 ):
     """Create an options object for how Marabou should solve the query
 
     Args:
@@ -153,4 +153,5 @@ def createOptions(numWorkers=1, initialTimeout=5, initialDivides=0, onlineDivide
     options._splittingStrategy = splittingStrategy
     options._sncSplittingStrategy = sncSplittingStrategy
     options._restoreTreeStates = restoreTreeStates
+    options._splitThreshold = splitThreshold
     return options
