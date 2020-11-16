0169039a77a063466ceb601bbd97e06cac2b3777
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-16 22:25:59 +0000

    Deploy NeuralNetworkVerification/Marabou to github.com/NeuralNetworkVerification/Marabou.git:gh-pages

---
+++ _downloads/3be0a9831aecbe3b95c8c903d8527042/4_DncExample.py
@@ -0,0 +1,34 @@
+'''
+DNC Example
+====================
+
+Top contributors (to current version):
+  - Andrew Wu
+  - Kyle Julian
+  
+This file is part of the Marabou project.
+Copyright (c) 2017-2019 by the authors listed in the file AUTHORS
+in the top-level source directory) and their institutional affiliations.
+All rights reserved. See the file COPYING in the top-level source
+directory for licensing information.
+'''
+
+import sys
+import numpy as np
+
+# Append to path if needed
+#sys.path.append(</Path/To/Marabou/>)
+
+from maraboupy import Marabou
+
+# %%
+# Load an example network and place an output constraint
+nnet_file_name = "../../src/input_parsers/acas_example/ACASXU_run2a_1_1_tiny_2.nnet"
+net = Marabou.read_nnet(nnet_file_name)
+net.setLowerBound(net.outputVars[0][0], .5)
+
+# %%
+# Solve the query with DNC mode turned on, which should return satisfying variable values
+options = Marabou.createOptions(snc=True, verbosity=0, initialDivides=2);
+vals, stats = net.solve(options=options)
+assert len(vals) > 0
---
+++ _sources/API/5_MarabouUtils.rst.txt
@@ -0,0 +1,5 @@
+MarabouUtils
+============
+
+.. automodule:: maraboupy.MarabouUtils
+   :members:
