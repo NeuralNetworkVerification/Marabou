3e3658b1dcd345629b4a67758f245300faa5e579
Matan Ostrovsky <matanost@gmail.com>
2021-01-07 01:00:19 +0200

    Update

--- build_marabou.sh
+++ build_marabou.sh
@@ -1,7 +1,13 @@
-export GUROBI_HOME="/cs/labs/guykatz/matanos/gurobi900/linux64"
-export PATH="${PATH}:${GUROBI_HOME}/bin"
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
-export GRB_LICENSE_FILE="/cs/share/etc/license/gurobi/gurobi.lic"
+#setenv GUROBI_HOME "/cs/labs/guykatz/matanos/gurobi900/linux64"
+#setenv PATH "${PATH}:${GUROBI_HOME}/bin"
+#setenv LD_LIBRARY_PATH "${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
+#setenv GRB_LICENSE_FILE "/cs/share/etc/license/gurobi/gurobi.lic"
+
+#export GUROBI_HOME="/cs/labs/guykatz/matanos/gurobi900/linux64"
+#export PATH="${PATH}:${GUROBI_HOME}/bin"
+#export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
+#export GRB_LICENSE_FILE="/cs/share/etc/license/gurobi/gurobi.lic"
+
 mkdir -p build
 cd build
 echo "$PWD"
