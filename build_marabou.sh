6cc926efc4175cbe6d071880b034c3f56c943e61
Matan Ostrovsky <matanost@gmail.com>
2021-01-06 20:41:50 +0200

    Gurobi infra fix

--- build_marabou.sh
+++ build_marabou.sh
@@ -1,9 +1,12 @@
-module load gurobi/9.0.0
-cd gkatz_space/$1
+export GUROBI_HOME="/cs/labs/guykatz/matanos/gurobi911"
+export PATH="${PATH}:${GUROBI_HOME}/bin"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
+export GRB_LICENSE_FILE="/cs/share/etc/license/gurobi/gurobi.lic"
 mkdir -p build
 cd build
 echo "$PWD"
-cmake ..
+#cmake ..
+cmake --build . -j 8 -DBUILD_PYTHON=ON -DENABLE_GUROBI=ON
 #cmake .. -DBUILD_PYTHON=ON -DENABLE_GUROBI=ON
 make
 
