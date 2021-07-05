a3b093ca1301e74af08b6938efcb54840bd2aaef
Andrew <haozewu@stanford.edu>
2021-07-05 11:51:41 -0700

    turn off logging

--- src/configuration/GlobalConfiguration.cpp
+++ src/configuration/GlobalConfiguration.cpp
@@ -102,12 +102,12 @@ const bool GlobalConfiguration::SMT_CORE_LOGGING = false;
 const bool GlobalConfiguration::DANTZIGS_RULE_LOGGING = false;
 const bool GlobalConfiguration::BASIS_FACTORIZATION_LOGGING = false;
 const bool GlobalConfiguration::PREPROCESSOR_LOGGING = false;
-const bool GlobalConfiguration::INPUT_QUERY_LOGGING = true;
+const bool GlobalConfiguration::INPUT_QUERY_LOGGING = false;
 const bool GlobalConfiguration::PROJECTED_STEEPEST_EDGE_LOGGING = false;
 const bool GlobalConfiguration::GAUSSIAN_ELIMINATION_LOGGING = false;
 const bool GlobalConfiguration::QUERY_LOADER_LOGGING = false;
 const bool GlobalConfiguration::SYMBOLIC_BOUND_TIGHTENER_LOGGING = false;
-const bool GlobalConfiguration::NETWORK_LEVEL_REASONER_LOGGING = true;
+const bool GlobalConfiguration::NETWORK_LEVEL_REASONER_LOGGING = false;
 const bool GlobalConfiguration::MPS_PARSER_LOGGING= false;
 
 const bool GlobalConfiguration::USE_SMART_FIX = false;
