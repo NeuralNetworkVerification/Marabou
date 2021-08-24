1a166d74f83bb8cee80014ec24930e1da99f8bba
Matan Ostrovsky <matanost@gmail.com>
2021-08-24 20:00:34 +0300

    bound dumping update

--- maraboupy/boundsDump.py
+++ maraboupy/boundsDump.py
@@ -1,4 +1,4 @@
-
+import random
 import sys
 import os
 import tensorflow as tf
@@ -9,11 +9,14 @@ import argparse
 import json
 
 from maraboupy import MarabouNetworkONNX as monnx
+from maraboupy import MarabouNetwork as mnet
 from maraboupy import MarabouCore
 from maraboupy import MarabouUtils
 from maraboupy import Marabou
 
 import matplotlib.pyplot as plt
+
+import gurobipy
 
 parser = argparse.ArgumentParser(description='Run MNIST based verification scheme using abstraction')
 parser.add_argument("--sample", type=int, default=0, help="Sample number")
@@ -29,11 +32,29 @@ x_train = np.expand_dims(x_train, -1)
 x_test = np.expand_dims(x_test, -1)
 
 xAdv = x_test[args.sample]
-mbouNet  = monnx.MarabouNetworkONNX(args.onnx)
-epsilon = args.dist
-for i,x in enumerate(np.nditer(xAdv)):
-    mbouNet.setUpperBound(i,max(x + epsilon, 0))    
-    mbouNet.setLowerBound(i,max(x - epsilon, 0))
+
+genMax = True
+if genMax:
+    mbouNet = mnet.MarabouNetwork()
+    maxIn = 4
+    mbouNet.numVars = maxIn + 1
+    mbouNet.addMaxConstraint({i for i in range(maxIn)}, maxIn)
+    mbouNet.inputVars = [np.array([i for i in range(maxIn)])]
+    mbouNet.outputVars = np.array([maxIn])
+    np.random.seed(0)
+    randBase = np.random.uniform(low=0., high=1., size=(maxIn,))
+    [mbouNet.setLowerBound(i, randBase[i] - 0.5) for i in range(maxIn)]
+    [mbouNet.setUpperBound(i, randBase[i] + 0.5) for i in range(maxIn)]
+    #mbouNet.setLowerBound(maxIn, -100)
+    #mbouNet.setUpperBound(maxIn,  100)
+    #[mbouNet.setLowerBound(i, 0. ) for i in range(maxIn)]
+    #[mbouNet.setUpperBound(i, 1. ) for i in range(maxIn)]
+else:
+    mbouNet  = monnx.MarabouNetworkONNX(args.onnx)
+    epsilon = args.dist
+    for i,x in enumerate(np.nditer(xAdv)):
+        mbouNet.setUpperBound(i,max(x + epsilon, 0))    
+        mbouNet.setLowerBound(i,max(x - epsilon, 0))
 
 options = Marabou.createOptions(verbosity=0, timeoutInSeconds=0, milpTightening=args.gurobi, dumpBounds=True, tighteningStrategy='none')
 ipq = MarabouCore.preprocess(mbouNet.getMarabouQuery(), options)
