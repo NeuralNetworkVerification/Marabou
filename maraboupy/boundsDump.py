09f543bf0f819203dd21b598fdbf5c15526c0334
Matan Ostrovsky <matanost@gmail.com>
2021-08-26 14:47:31 +0300

    Update - seems to find example of Max LP Relaxation

--- maraboupy/boundsDump.py
+++ maraboupy/boundsDump.py
@@ -24,7 +24,6 @@ parser.add_argument("--dist", type=float, default=0.03, help="Property distance"
 parser.add_argument("--onnx", type=str, default='fullVanilla.onnx', help="Onnx net file")
 parser.add_argument("--gurobi", type=str, default='lp', choices=['lp', 'milp'], help="Gurbi solver type")
 parser.add_argument("--flag", type=str, default='', help="Addition to the dump file's name")
-parser.add_argument("--genMax", action="store_true", default=False, help="genMax")
 args = parser.parse_args()
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
@@ -32,54 +31,83 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 x_train = np.expand_dims(x_train, -1)
 x_test = np.expand_dims(x_test, -1)
 
-xAdv = x_test[args.sample]
+xAdv = x_test[args.sample]        
 
-genMax = args.genMax
+options = Marabou.createOptions(verbosity=2, timeoutInSeconds=0, milpTightening='lp', dumpBounds=True, tighteningStrategy='none', milpSolverTimeout=100)
 
-if genMax:
-    mbouNet = mnet.MarabouNetwork()
-    maxIn = 16
-    numOut = 8
-    mbouNet.numVars = maxIn + numOut + (numOut - 1)    
-    [mbouNet.addMaxConstraint({i + j for i in range(maxIn)}, maxIn + numOut + j - 1) for j in range(numOut)] 
-    mbouNet.inputVars = [np.array([i for i in range(maxIn + numOut - 1)])]
-    mbouNet.outputVars = np.array([maxIn + numOut + i - 1 for i in range(numOut)])
-    #np.random.seed(0)
-    randBase = np.random.uniform(low=0., high=1., size=(maxIn + numOut - 1,))
-    randNoiseL = np.random.normal(loc=1., size=(maxIn + numOut - 1,))
-    randNoiseU = np.random.normal(loc=1., size=(maxIn + numOut - 1,))
-    [mbouNet.setLowerBound(i, randBase[i] - randNoiseL[i]) for i in range(maxIn + numOut - 1)]
-    [mbouNet.setUpperBound(i, randBase[i] + randNoiseU[i]) for i in range(maxIn + numOut - 1)]
-    firstOut = np.min(mbouNet.outputVars)
-    for out in np.nditer(mbouNet.outputVars):        
-        if out != firstOut:
-            mbouNet.addInequality([out, firstOut], [1, -1], 0.)    
-    #mbouNet.setLowerBound(maxIn, -100)
-    #mbouNet.setUpperBound(maxIn,  100)
-    #[mbouNet.setLowerBound(i, 0. ) for i in range(maxIn)]
-    #[mbouNet.setUpperBound(i, 1. ) for i in range(maxIn)]
-else:
-    mbouNet  = monnx.MarabouNetworkONNX(args.onnx)
-    epsilon = args.dist
-    for i,x in enumerate(np.nditer(xAdv)):
-        mbouNet.setUpperBound(i,max(x + epsilon, 0))    
-        mbouNet.setLowerBound(i,max(x - epsilon, 0))
-    firstOut = np.min(mbouNet.outputVars)
-    for out in np.nditer(mbouNet.outputVars):        
-        if out != firstOut:
-            mbouNet.addInequality([out, firstOut], [1, -1], 0.)
-        
-
-options = Marabou.createOptions(verbosity=0, timeoutInSeconds=0, milpTightening=args.gurobi, dumpBounds=True, tighteningStrategy='none')
+print("\n\n\n**************** Max 1 ****************")
+mbouNet = mnet.MarabouNetwork()
+mbouNet.numVars = 7
+[mbouNet.setLowerBound(i, -1.) for i in range(1)]
+[mbouNet.setUpperBound(i, 1.) for i in range(1)]
+mbouNet.setLowerBound(5, -0.5)
+mbouNet.setUpperBound(5, 0.5)
+mbouNet.inputVars = [np.array([0,5])]
+mbouNet.addEquality([0, 1], [1, -1], -0.5)
+mbouNet.addEquality([0, 2], [3, -1], -0.5)    
+mbouNet.addRelu(1,3)
+mbouNet.addRelu(2,4)
+mbouNet.addMaxConstraint({3,4,5}, 6)
+mbouNet.outputVars = np.array([6])
 ipq = MarabouCore.preprocess(mbouNet.getMarabouQuery(), options)
-print("Number of variables in IPQ={}".format(ipq.getNumberOfVariables()))
+if ipq.getNumberOfVariables() == 0:
+    #print("UNSAT on first LP bound tightening")
+    pass
+else:
+    maxUB = max([ipq.getUpperBound(j) for j in [3, 4, 5]])
+    if round(maxUB,4) != round(ipq.getUpperBound(6),4):
+        print("maxUB = {}".format(maxUB))
+        print("ipq.getUpperBound(6) = {}".format(ipq.getUpperBound(6)))        
+
+
+print("\n\n\n**************** Sign ****************")
+mbouNet = mnet.MarabouNetwork()
+mbouNet.numVars = 6
+mbouNet.setLowerBound(0, -1)
+mbouNet.setUpperBound(0, 1)
+mbouNet.inputVars = [np.array([0])]
+mbouNet.addEquality([0, 1], [-3, 1], 1)
+mbouNet.addEquality([0, 2], [4, 1], 2)
+mbouNet.addSignConstraint(1,3)
+mbouNet.addSignConstraint(2,4)
+mbouNet.addEquality([3, 4, 5], [-1, -1, 1], 0)
+mbouNet.outputVars = np.array([5])
+ipq = MarabouCore.preprocess(mbouNet.getMarabouQuery(), options)
+if ipq.getNumberOfVariables() == 0:
+    pass
+else:
+    maxUB = max([ipq.getUpperBound(j) for j in [3, 4, 5]])
+    if round(maxUB,4) != round(ipq.getUpperBound(5),4):
+        print("maxUB = {}".format(maxUB))
+        print("ipq.getUpperBound(5) = {}".format(ipq.getUpperBound(5)))
+
+        
+print("\n\n\n**************** Max 2 ****************")
+mbouNet = mnet.MarabouNetwork()
+mbouNet.numVars = 10
+mbouNet.setLowerBound(0, -1.)
+mbouNet.setUpperBound(0,  1.)
+mbouNet.setLowerBound(1, -1.)
+mbouNet.setUpperBound(1,  1.)
+mbouNet.setLowerBound(2, -1.)
+mbouNet.setUpperBound(2,  1.)
+mbouNet.setLowerBound(3, -1.)
+mbouNet.setUpperBound(3,  1.)
+mbouNet.inputVars = [np.array(list(range(4)))]
+mbouNet.addEquality([0, 1, 4], [-1, 1, 1], 0)
+mbouNet.addEquality([1, 2, 5], [-1, 1, 1], 0)
+mbouNet.addEquality([2, 3, 6], [-1, 1, 1], 0)
+mbouNet.addMaxConstraint({4,5}, 7)
+mbouNet.addMaxConstraint({5,6}, 8)
+mbouNet.addEquality([7, 8, 9], [-1, -1, 1], 0)
+mbouNet.outputVars = np.array([9])
+ipq = MarabouCore.preprocess(mbouNet.getMarabouQuery(), options)
 if ipq.getNumberOfVariables() == 0:
     print("UNSAT on first LP bound tightening")
-    exit()
 else:
-    newName = "dumpBounds_{}_{}{}.json".format(args.sample, str(args.dist).replace('.','-'), "_" + args.flag if args.flag else "")
-    os.rename("dumpBounds.json", newName)
-    print("dumped={}".format(newName))
+    pass
+#    maxUB = max([ipq.getUpperBound(j) for j in [0,1,2]])
+#    if round(maxUB,4) != round(ipq.getUpperBound(5),4):
+#        print("maxUB = {}".format(maxUB))
+#        print("ipq.getUpperBound(5) = {}".format(ipq.getUpperBound(5)))
 
-if genMax:
-    print("max UB={}".format(max([randBase[i] + randNoiseU[i] for i in range(maxIn)])))    
