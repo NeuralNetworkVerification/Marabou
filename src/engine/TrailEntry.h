7dbd15627e10e94b069898f8996bf09f07f945de
Aleksandar Zeljic <zeljic@stanford.edu>
2021-10-22 15:37:08 -0700

    Add TrailEntry class

---
+++ src/engine/TrailEntry.h
@@ -0,0 +1,56 @@
+/*********************                                                        */
+/*! \file TrailEntry.h
+** \verbatim
+** Top contributors (to current version):
+**  Aleksandar Zeljic
+** This file is part of the Marabou project.
+** Copyright (c) 2017-2019 by the authors listed in the file AUTHORS
+** in the top-level source directory) and their institutional affiliations.
+** All rights reserved. See the file COPYING in the top-level source
+** directory for licensing information.\endverbatim
+**
+** [[ Add lengthier description here ]]
+**/
+
+#ifndef __TrailEntry_h__
+#define __TrailEntry_h__
+
+#include "PiecewiseLinearCaseSplit.h"
+
+/*
+  A trail entry consists of the pointer to a PiecewiseLinearConstraint and
+  phase designation.
+*/
+class TrailEntry
+{
+public:
+  PiecewiseLinearConstraint * _pwlConstraint;
+  PhaseStatus _phase;
+
+  PiecewiseLinearCaseSplit getPiecewiseLinearCaseSplit() const
+  {
+      return _pwlConstraint->getCaseSplit( _phase );
+  }
+
+  inline void markInfeasible()
+  {
+      _pwlConstraint->markInfeasible( _phase );
+  }
+
+ TrailEntry(PiecewiseLinearConstraint * pwlc, PhaseStatus phase )
+     : _pwlConstraint( pwlc )
+     , _phase( phase )
+    {}
+
+  ~TrailEntry() {};
+};
+
+#endif // TrailEntry.h
+
+//
+// Local Variables:
+// compile-command: "make -C ../.. "
+// tags-file-name: "../../TAGS"
+// c-basic-offset: 4
+// End:
+//
