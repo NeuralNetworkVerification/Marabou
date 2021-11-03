0c1eda9d3b4748b4f0bd0d23a3c2400aceb17ae5
Matan Ostrovsky <matanost@gmail.com>
2021-11-03 22:18:00 +0200

    update dir

--- maraboupy/evaluation/downloadMnist.py
+++ maraboupy/evaluation/downloadMnist.py
@@ -3,8 +3,11 @@ import sys
 import os
 import tensorflow as tf
 import numpy as np
+import CnnAbs
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+os.chdir(CnnAbs.CnnAbs.maraboupyPath)
+os.makedirs('mnist')
 
 with open('mnist/x_train.npy', 'wb') as f:
             np.save(f, x_train)
@@ -13,6 +16,6 @@ with open('mnist/y_train.npy', 'wb') as f:
 with open('mnist/x_test.npy', 'wb') as f:
             np.save(f, x_test)
 with open('mnist/y_test.npy', 'wb') as f:
-    np.save(f, y_test)
+            np.save(f, y_test)
 
 
