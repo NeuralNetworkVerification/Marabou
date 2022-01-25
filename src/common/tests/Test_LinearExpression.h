959f0770d4929f6b06a8d1dac889db86c9211346
Haoze(Andrew) Wu <haozewu@stanford.edu>
2022-01-24 22:00:50 -0800

    Update Test_LinearExpression.h

--- src/common/tests/Test_LinearExpression.h
+++ src/common/tests/Test_LinearExpression.h
@@ -47,11 +47,3 @@ public:
         TS_ASSERT_EQUALS( expr.evaluate( assignment ), -9.5);
     }
 };
-
-//
-// Local Variables:
-// compile-command: "make -C ../../.. "
-// tags-file-name: "../../../TAGS"
-// c-basic-offset: 4
-// End:
-//
