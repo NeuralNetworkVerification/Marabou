3dc33ecc928ac7f1e726f8746890aa495e820587
Haoze(Andrew) Wu <haozewu@stanford.edu>
2022-01-25 00:00:56 -0800

    Update SoISearchStrategy.h

--- src/engine/SoISearchStrategy.h
+++ src/engine/SoISearchStrategy.h
@@ -27,11 +27,3 @@ enum class SoISearchStrategy
 };
 
 #endif // __SoISearchStrategy_h__
-
-//
-// Local Variables:
-// compile-command: "make -C ../.. "
-// tags-file-name: "../../TAGS"
-// c-basic-offset: 4
-// End:
-//
