f792e01fac78c00f62a0abe13ebafe3904521831
anwu1219 <haozewu@stanford.edu>
2022-02-14 21:19:20 -0800

    solve lp with gurobi

--- maraboupy/Marabou.py
+++ maraboupy/Marabou.py
@@ -123,8 +123,9 @@ def createOptions(numWorkers=1, initialTimeout=5, initialSplits=0, onlineSplits=
                   splittingStrategy="auto", sncSplittingStrategy="auto",
                   restoreTreeStates=False, splitThreshold=20, solveWithMILP=False,
                   preprocessorBoundTolerance=0.0000000001, dumpBounds=False,
-                  tighteningStrategy="deeppoly", milpTightening="lp", milpSolverTimeout=0,
-                  numSimulations=10, skipLpTighteningAfterSplit=False, numBlasThreads=1):
+                  tighteningStrategy="deeppoly", milpTightening="none", milpSolverTimeout=0,
+                  numSimulations=10, skipLpTighteningAfterSplit=False, numBlasThreads=1,
+                  performLpTighteningAfterSplit=False):
     """Create an options object for how Marabou should solve the query
 
     Args:
@@ -150,6 +151,7 @@ def createOptions(numWorkers=1, initialTimeout=5, initialSplits=0, onlineSplits=
         numSimulations (int, optional): Number of simulations generated per neuron, defaults to 10
         skipLpTighteningAfterSplit (bool, optional): Whether to skip a LP tightening after a case split, defaults to False
         numBlasThreads (int, optional): Number of threads to use when using OpenBLAS matrix multiplication (e.g., for DeepPoly analysis), defaults to 1
+        performLpTighteningAfterSplit (bool, optional): Whether to perform a LP tightening after a case split, defaults to False
     Returns:
         :class:`~maraboupy.MarabouCore.Options`
     """
@@ -173,6 +175,6 @@ def createOptions(numWorkers=1, initialTimeout=5, initialSplits=0, onlineSplits=
     options._milpTightening = milpTightening
     options._milpSolverTimeout = milpSolverTimeout
     options._numSimulations = numSimulations
-    options._skipLpTighteningAfterSplit = skipLpTighteningAfterSplit
     options._numBlasThreads = numBlasThreads
+    options._performLpTighteningAfterSplit = performLpTighteningAfterSplit
     return options
