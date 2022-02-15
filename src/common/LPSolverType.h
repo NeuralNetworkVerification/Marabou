8ba16540aceb790d7cdfc3659afb67478fd9a017
anwu1219 <haozewu@stanford.edu>
2022-02-14 21:18:38 -0800

    add file

---
+++ src/common/LPSolverType.h
@@ -0,0 +1,29 @@
+/*********************                                                        */
+/*! \file LPSolverType.h
+** \verbatim
+** Top contributors (to current version):
+**   Haoze Wu
+** This file is part of the Marabou project.
+** Copyright (c) 2017-2019 by the authors listed in the file AUTHORS
+** in the top-level source directory) and their institutional affiliations.
+** All rights reserved. See the file COPYING in the top-level source
+** directory for licensing information.\endverbatim
+**
+** [[ Add lengthier description here ]]
+
+**/
+
+#ifndef __LPSolverType_h__
+#define __LPSolverType_h__
+
+enum class LPSolverType
+{
+    // Native simplex implementation. Open-sourced, more agressive
+    // bound derivation, and will support proof-production.
+    NATIVE,
+
+    // Not open-sourced. Use this for optimal run-time performance.
+    GUROBI,
+};
+
+#endif // __LPSolverType_h__
