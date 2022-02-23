7fbf71ec1314cdf50fa84781d7d1b271a31d8b2a
Aleksandar Zeljic <zeljic@stanford.edu>
2022-02-23 12:04:14 -0800

    Initialize BoundManager with preprocessed bounds

--- src/engine/Engine.cpp
+++ src/engine/Engine.cpp
@@ -1322,6 +1322,13 @@ bool Engine::processInputQuery( InputQuery &inputQuery, bool preprocess )
                 plConstraint->addAuxiliaryEquationsAfterPreprocessing
                     ( *_preprocessedQuery );
 
+        _boundManager.initialize( _preprocessedQuery.getNumberOfVariables() );
+        for ( unsigned i = 0; i < _preprocessedQuery.getNumberOfVariables(); ++i )
+        {
+            _boundManager.setLowerBound( i, _preprocessedQuery.getLowerBound( i ) );
+            _boundManager.setUpperBound( i, _preprocessedQuery.getUpperBound( i ) );
+        }
+
         if ( _lpSolverType == LPSolverType::NATIVE )
         {
             double *constraintMatrix = createConstraintMatrix();
