30fc1d944a062d775b7ac93edbc7019ad3306b50
anwu1219 <haozewu@stanford.edu>
2022-03-01 15:01:48 -0800

    clean up

--- src/configuration/OptionParser.cpp
+++ src/configuration/OptionParser.cpp
@@ -50,12 +50,12 @@ void OptionParser::initialize()
         ( "property",
           boost::program_options::value<std::string>( &(*_stringOptions)[Options::PROPERTY_FILE_PATH] )->default_value( (*_stringOptions)[Options::PROPERTY_FILE_PATH] ),
           "Property file." )
+        ( "input-query",
+          boost::program_options::value<std::string>( &((*_stringOptions)[Options::INPUT_QUERY_FILE_PATH]) )->default_value( (*_stringOptions)[Options::INPUT_QUERY_FILE_PATH] ),
+          "Input Query file" )
         ( "portfolio",
           boost::program_options::bool_switch( &(*_boolOptions)[Options::PORTFOLIO_MODE] )->default_value( (*_boolOptions)[Options::PORTFOLIO_MODE] ),
           "Use the portfolio solving mode." )
-        ( "query-dump-file",
-          boost::program_options::value<std::string>( &(*_stringOptions)[Options::QUERY_DUMP_FILE] )->default_value( (*_stringOptions)[Options::QUERY_DUMP_FILE] ),
-          "(string) Dump the verification query in Marabou's input query format." )
         ( "num-workers",
           boost::program_options::value<int>( &(*_intOptions)[Options::NUM_WORKERS] )->default_value( (*_intOptions)[Options::NUM_WORKERS] ),
           "Number of threads to use." )
@@ -68,87 +68,90 @@ void OptionParser::initialize()
 #ifdef ENABLE_GUROBI
         ( "milp",
           boost::program_options::bool_switch( &(*_boolOptions)[Options::SOLVE_WITH_MILP] )->default_value( (*_boolOptions)[Options::SOLVE_WITH_MILP] ),
-          "Use a MILP solver to solve the input query" )
+          "Solve the input query with a MILP encoding in Gruobi." )
 #endif
         ;
 
     // Less common options
     _other.add_options()
-        ( "dump-bounds",
-          boost::program_options::bool_switch( &((*_boolOptions)[Options::DUMP_BOUNDS]) )->default_value( (*_boolOptions)[Options::DUMP_BOUNDS] ),
-          "Dump the bounds after preprocessing" )
+        ( "verbosity",
+          boost::program_options::value<int>( &((*_intOptions)[Options::VERBOSITY]) )->default_value( (*_intOptions)[Options::VERBOSITY] ),
+          "Verbosity of engine::solve(). 0: does not print anything, 1: print"
+          "out statistics in the beginning and end, 2: print out statistics during solving." )
         ( "snc",
           boost::program_options::bool_switch( &((*_boolOptions)[Options::DNC_MODE]) )->default_value( (*_boolOptions)[Options::DNC_MODE] ),
           "Use the split-and-conquer solving mode" )
-        ( "summary-file",
-          boost::program_options::value<std::string>( &((*_stringOptions)[Options::SUMMARY_FILE]) )->default_value( (*_stringOptions)[Options::SUMMARY_FILE] ),
-          "Summary file" )
-        ( "verbosity",
-          boost::program_options::value<int>( &((*_intOptions)[Options::VERBOSITY]) )->default_value( (*_intOptions)[Options::VERBOSITY] ),
-          "Verbosity of engine::solve(). 0: does not print anything (for SnC), 1: print"
-          "out statistics in the beginning and end, 2: print out statistics during solving." )
         ( "seed",
           boost::program_options::value<int>( &((*_intOptions)[Options::SEED]) )->default_value( (*_intOptions)[Options::SEED] ),
           "The random seed." )
+        ( "dump-bounds",
+          boost::program_options::bool_switch( &((*_boolOptions)[Options::DUMP_BOUNDS]) )->default_value( (*_boolOptions)[Options::DUMP_BOUNDS] ),
+          "Dump the bounds after preprocessing" )
+        ( "query-dump-file",
+          boost::program_options::value<std::string>( &(*_stringOptions)[Options::QUERY_DUMP_FILE] )->default_value( (*_stringOptions)[Options::QUERY_DUMP_FILE] ),
+          "Dump the verification query in Marabou's input query format." )
+        ( "summary-file",
+          boost::program_options::value<std::string>( &((*_stringOptions)[Options::SUMMARY_FILE]) )->default_value( (*_stringOptions)[Options::SUMMARY_FILE] ),
+          "Produce a summary file of the run." )
 #ifdef ENABLE_GUROBI
 #endif // ENABLE_GUROBI
         ;
 
     _expert.add_options()
-        ( "branch",
-          boost::program_options::value<std::string>( &((*_stringOptions)[Options::SPLITTING_STRATEGY]) ),
-          "The branching strategy (earliest-relu/pseudo-impact/largest-interval/relu-violation/polarity)" )
-        ( "blas-threads",
-          boost::program_options::value<int>( &((*_intOptions)[Options::NUM_BLAS_THREADS]) ),
-          "Number of threads to use for matrix multiplication with OpenBLAS" )
-        ( "restore-tree-states",
-          boost::program_options::bool_switch( &((*_boolOptions)[Options::RESTORE_TREE_STATES]) ),
-          "Restore tree states in SnC mode" )
-        ( "soi-search-strategy",
-          boost::program_options::value<std::string>( &((*_stringOptions)[Options::SOI_SEARCH_STRATEGY]) ),
-          "Strategy for stochastically minimizing the soi: mcmc/walksat. default: mcmc" )
-        ( "soi-init-strategy",
-          boost::program_options::value<std::string>( &((*_stringOptions)[Options::SOI_INITIALIZATION_STRATEGY]) ),
-          "Strategy for initialize the soi function: input-assignment/current-assignment. default: input-assignment" )
-        ( "split-strategy",
-          boost::program_options::value<std::string>( &((*_stringOptions)[Options::SNC_SPLITTING_STRATEGY]) ),
-          "(SnC) The splitting strategy" )
         ( "tightening-strategy",
-          boost::program_options::value<std::string>( &((*_stringOptions)[Options::SYMBOLIC_BOUND_TIGHTENING_TYPE]) ),
-          "type of bound tightening technique to use: sbt/deeppoly/none. default: deeppoly" )
+          boost::program_options::value<std::string>( &((*_stringOptions)[Options::SYMBOLIC_BOUND_TIGHTENING_TYPE]) )->default_value( (*_stringOptions)[Options::SYMBOLIC_BOUND_TIGHTENING_TYPE] ),
+          "type of bound tightening technique to use: sbt/deeppoly/none." )
+        ( "branch",
+          boost::program_options::value<std::string>( &((*_stringOptions)[Options::SPLITTING_STRATEGY]) )->default_value( (*_stringOptions)[Options::SPLITTING_STRATEGY] ),
+          "The branching strategy (earliest-relu/pseudo-impact/largest-interval/relu-violation/polarity)." )
+        ( "soi-split-threshold",
+          boost::program_options::value<int>( &((*_intOptions)[Options::DEEP_SOI_REJECTION_THRESHOLD]) )->default_value( (*_intOptions)[Options::DEEP_SOI_REJECTION_THRESHOLD] ),
+          "(DeepSoI) Max number of rejected phase pattern proposal before splitting" )
+        ( "soi-search-strategy",
+          boost::program_options::value<std::string>( &((*_stringOptions)[Options::SOI_SEARCH_STRATEGY]) )->default_value( (*_stringOptions)[Options::SOI_SEARCH_STRATEGY] ),
+          "(DeepSoI) Strategy for stochastically minimizing the soi: mcmc/walksat." )
+        ( "soi-init-strategy",
+          boost::program_options::value<std::string>( &((*_stringOptions)[Options::SOI_INITIALIZATION_STRATEGY]) )->default_value( (*_stringOptions)[Options::SOI_INITIALIZATION_STRATEGY] ),
+          "(DeepSoI) Strategy for initialize the soi function: input-assignment/current-assignment. default: input-assignment" )
+        ( "mcmc-beta",
+          boost::program_options::value<float>( &((*_floatOptions)[Options::PROBABILITY_DENSITY_PARAMETER]) )->default_value( (*_floatOptions)[Options::PROBABILITY_DENSITY_PARAMETER] ),
+          "(DeepSoI) The beta parameter in MCMC search." )
+        ( "split-strategy",
+          boost::program_options::value<std::string>( &((*_stringOptions)[Options::SNC_SPLITTING_STRATEGY]) )->default_value( (*_stringOptions)[Options::SNC_SPLITTING_STRATEGY] ),
+          "(SnC) The splitting strategy" )
         ( "initial-divides",
-          boost::program_options::value<int>( &((*_intOptions)[Options::NUM_INITIAL_DIVIDES]) ),
+          boost::program_options::value<int>( &((*_intOptions)[Options::NUM_INITIAL_DIVIDES]) )->default_value( (*_intOptions)[Options::NUM_INITIAL_DIVIDES] ),
           "(SnC) Number of times to initially bisect the input region" )
         ( "initial-timeout",
-          boost::program_options::value<int>( &((*_intOptions)[Options::INITIAL_TIMEOUT]) ),
+          boost::program_options::value<int>( &((*_intOptions)[Options::INITIAL_TIMEOUT]) )->default_value( (*_intOptions)[Options::INITIAL_TIMEOUT] ),
           "(SnC) The initial timeout" )
         ( "num-online-divides",
-          boost::program_options::value<int>( &((*_intOptions)[Options::NUM_ONLINE_DIVIDES]) ),
+          boost::program_options::value<int>( &((*_intOptions)[Options::NUM_ONLINE_DIVIDES]) )->default_value( (*_intOptions)[Options::NUM_ONLINE_DIVIDES] ),
           "(SnC) Number of times to further bisect a sub-region when a timeout occurs" )
-        ( "reluplex-split-threshold",
-          boost::program_options::value<int>( &((*_intOptions)[Options::CONSTRAINT_VIOLATION_THRESHOLD]) ),
-          "Max number of tries to repair a relu before splitting" )
-        ( "soi-split-threshold",
-          boost::program_options::value<int>( &((*_intOptions)[Options::DEEP_SOI_REJECTION_THRESHOLD]) ),
-          "Max number of rejected phase pattern proposal before splitting" )
         ( "timeout-factor",
-          boost::program_options::value<float>( &((*_floatOptions)[Options::TIMEOUT_FACTOR]) ),
+          boost::program_options::value<float>( &((*_floatOptions)[Options::TIMEOUT_FACTOR]) )->default_value( (*_floatOptions)[Options::TIMEOUT_FACTOR] ),
           "(SnC) The timeout factor" )
+        ( "restore-tree-states",
+          boost::program_options::bool_switch( &((*_boolOptions)[Options::RESTORE_TREE_STATES]) )->default_value( (*_boolOptions)[Options::RESTORE_TREE_STATES] ),
+          "(SnC) Restore tree states in SnC mode" )
+        ( "blas-threads",
+          boost::program_options::value<int>( &((*_intOptions)[Options::NUM_BLAS_THREADS]) )->default_value( (*_intOptions)[Options::NUM_BLAS_THREADS] ),
+          "Number of threads to use for matrix multiplication with OpenBLAS" )
+        ( "reluplex-split-threshold",
+          boost::program_options::value<int>( &((*_intOptions)[Options::CONSTRAINT_VIOLATION_THRESHOLD]) )->default_value( (*_intOptions)[Options::CONSTRAINT_VIOLATION_THRESHOLD] ),
+          "Max number of tries to repair a relu before splitting" )
         ( "preprocessor-bound-tolerance",
-          boost::program_options::value<float>( &((*_floatOptions)[Options::PREPROCESSOR_BOUND_TOLERANCE]) ),
+          boost::program_options::value<float>( &((*_floatOptions)[Options::PREPROCESSOR_BOUND_TOLERANCE]) )->default_value( (*_floatOptions)[Options::PREPROCESSOR_BOUND_TOLERANCE] ),
           "epsilon for preprocessor bound tightening comparisons" )
-        ( "mcmc-beta",
-          boost::program_options::value<float>( &((*_floatOptions)[Options::PROBABILITY_DENSITY_PARAMETER]) ),
-          "beta parameter in MCMC search." )
 #ifdef ENABLE_GUROBI
         ( "lp-solver",
-          boost::program_options::value<std::string>( &((*_stringOptions)[Options::LP_SOLVER]) ),
-          "Solver for the LPs during the complete analysis: native/gurobi. default: gurobi" )
+          boost::program_options::value<std::string>( &((*_stringOptions)[Options::LP_SOLVER]) )->default_value( (*_stringOptions)[Options::LP_SOLVER] ),
+          "Solver for the LPs during the complete analysis: native/gurobi." )
         ( "num-simulations",
-          boost::program_options::value<int>( &((*_intOptions)[Options::NUMBER_OF_SIMULATIONS]) ),
+          boost::program_options::value<int>( &((*_intOptions)[Options::NUMBER_OF_SIMULATIONS]) )->default_value( (*_intOptions)[Options::NUMBER_OF_SIMULATIONS] ),
           "Number of simulations generated per neuron" )
         ( "lp-tightening-after-split",
-          boost::program_options::bool_switch( &((*_boolOptions)[Options::PERFORM_LP_TIGHTENING_AFTER_SPLIT]) ),
+          boost::program_options::bool_switch( &((*_boolOptions)[Options::PERFORM_LP_TIGHTENING_AFTER_SPLIT]) )->default_value( (*_boolOptions)[Options::PERFORM_LP_TIGHTENING_AFTER_SPLIT] ),
           "Whether to skip a LP tightening after a case split" )
         ( "milp-timeout",
           boost::program_options::value<float>( &((*_floatOptions)[Options::MILP_SOLVER_TIMEOUT]) )->default_value( (*_floatOptions)[Options::MILP_SOLVER_TIMEOUT] ),
@@ -189,7 +192,7 @@ int OptionParser::extractIntValue( const String &option )
 void OptionParser::printHelpMessage() const
 {
     std::cerr << "\nusage: ./Marabou <network.nnet> <property> [<options>]\n" << std::endl;
-    std::cerr <<  "OR      ./Marabou --input-query <input-query-file> [<options>]\n" << std::endl;
+    std::cerr <<  "OR     ./Marabou --input-query <input-query-file> [<options>]\n" << std::endl;
 
     std::cerr << "You might also consider using the ./resources/runMarabou.py "
               << "script, see README.md for more information." << std::endl;
