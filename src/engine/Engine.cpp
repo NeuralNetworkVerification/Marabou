4a7558503a4d6e4da40d80a0f8a9c3bd5142979a
Aleksandar Zeljic <zeljic@stanford.edu>
2022-03-02 13:33:49 -0800

    Initialize BoundManager using preprocessed bounds

--- src/engine/Engine.cpp
+++ src/engine/Engine.cpp
@@ -1343,6 +1343,14 @@ bool Engine::processInputQuery( InputQuery &inputQuery, bool preprocess )
             delete[] constraintMatrix;
             constraintMatrix = createConstraintMatrix();
 
+            _boundManager.initialize( _preprocessedQuery.getNumberOfVariables() );
+            for ( unsigned i = 0; i < _preprocessedQuery.getNumberOfVariables(); ++i )
+            {
+                _boundManager.setLowerBound( i, _preprocessedQuery.getLowerBound( i ) );
+                _boundManager.setUpperBound( i, _preprocessedQuery.getUpperBound( i ) );
+            }
+
+
             initializeTableau( constraintMatrix, initialBasis );
 
             delete[] constraintMatrix;
