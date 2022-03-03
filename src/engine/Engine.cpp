f3a7d2c396332018839a2120df0cecee301a1f99
Haoze(Andrew) Wu <haozewu@stanford.edu>
2022-03-03 15:22:10 -0800

    Update Engine.cpp

--- src/engine/Engine.cpp
+++ src/engine/Engine.cpp
@@ -77,6 +77,7 @@ Engine::Engine()
     _activeEntryStrategy = _projectedSteepestEdgeRule;
     _activeEntryStrategy->setStatistics( &_statistics );
     _statistics.stampStartingTime();
+    setRandomSeed( Options::get()->getInt( Options::SEED ) );
 }
 
 Engine::~Engine()
