42ecf37e2d060beca3ff484cf7e97521dc65c73b
OmriIsacHUJI <99890010+OmriIsacHUJI@users.noreply.github.com>
2022-03-15 17:25:35 +0200

    Update BoundExplainer.cpp
    Row coefficients vector is added with coefficient 1.
    Minor modifications.

--- src/proofs/BoundExplainer.cpp
+++ src/proofs/BoundExplainer.cpp
@@ -92,7 +92,7 @@ void BoundExplainer::updateBoundExplanation( const TableauRow &row, bool isUpper
 
         // If we're currently explaining an upper bound, we use upper bound explanation iff variable's coefficient is positive
         // If we're currently explaining a lower bound, we use upper bound explanation iff variable's coefficient is negative
-        tempUpper = ( isUpper && realCoefficient > 0 ) || ( !isUpper &&  realCoefficient < 0 );
+        tempUpper = ( isUpper && realCoefficient > 0 ) || ( !isUpper && realCoefficient < 0 );
         tempBound = tempUpper ? _upperBoundExplanations[curVar] : _lowerBoundExplanations[curVar];
         addVecTimesScalar( sum, tempBound, realCoefficient );
     }
@@ -103,7 +103,7 @@ void BoundExplainer::updateBoundExplanation( const TableauRow &row, bool isUpper
         realCoefficient = 1 / ci;
         if ( !FloatUtils::isZero( realCoefficient ) )
         {
-            tempUpper = ( isUpper && realCoefficient > 0 ) || ( !isUpper &&  realCoefficient < 0 );
+            tempUpper = ( isUpper && realCoefficient > 0 ) || ( !isUpper && realCoefficient < 0 );
             tempBound = tempUpper ? _upperBoundExplanations[row._lhs] : _lowerBoundExplanations[row._lhs];
             addVecTimesScalar( sum, tempBound, realCoefficient );
         }
@@ -113,7 +113,7 @@ void BoundExplainer::updateBoundExplanation( const TableauRow &row, bool isUpper
     extractRowCoefficients( row, rowCoefficients, ci );
     addVecTimesScalar( sum, rowCoefficients, 1 );
 
-    injectExplanation( sum, var, isUpper );
+    setExplanation( sum, var, isUpper );
 }
 
 void BoundExplainer::updateBoundExplanationSparse( const SparseUnsortedList &row, bool isUpper, unsigned var )
@@ -154,7 +154,7 @@ void BoundExplainer::updateBoundExplanationSparse( const SparseUnsortedList &row
 
         // If we're currently explaining an upper bound, we use upper bound explanation iff variable's coefficient is positive
         // If we're currently explaining a lower bound, we use upper bound explanation iff variable's coefficient is negative
-        tempUpper = ( isUpper && realCoefficient > 0 ) || ( !isUpper &&  realCoefficient < 0 );
+        tempUpper = ( isUpper && realCoefficient > 0 ) || ( !isUpper && realCoefficient < 0 );
         tempBound = tempUpper ? _upperBoundExplanations[entry._index] : _lowerBoundExplanations[entry._index];
         addVecTimesScalar( sum, tempBound, realCoefficient );
     }
@@ -163,7 +163,7 @@ void BoundExplainer::updateBoundExplanationSparse( const SparseUnsortedList &row
     extractSparseRowCoefficients( row, rowCoefficients, ci );
     addVecTimesScalar( sum, rowCoefficients, 1 );
 
-    injectExplanation( sum, var, isUpper );
+    setExplanation( sum, var, isUpper );
 }
 
 void BoundExplainer::addVecTimesScalar( Vector<double> &sum, const Vector<double> &input,  double scalar ) const
@@ -173,36 +173,37 @@ void BoundExplainer::addVecTimesScalar( Vector<double> &sum, const Vector<double
 
     ASSERT( sum.size() == _numberOfRows && input.size() == _numberOfRows );
 
-    for (unsigned i = 0; i < _numberOfRows; ++i )
+    for ( unsigned i = 0; i < _numberOfRows; ++i )
         sum[i] += scalar * input[i];
 }
 
 void BoundExplainer::extractRowCoefficients( const TableauRow &row, Vector<double> &coefficients, double ci ) const
 {
     ASSERT( coefficients.size() == _numberOfRows && row._size <= _numberOfVariables );
+    ASSERT( !FloatUtils::isZero( ci ) );
 
     // The coefficients of the row m highest-indices vars are the coefficients of slack variables
     for ( unsigned i = 0; i < row._size; ++i )
     {
-        if (row._row[i]._var >= _numberOfVariables - _numberOfRows && !FloatUtils::isZero(row._row[i]._coefficient ) )
-            coefficients[row._row[i]._var - _numberOfVariables + _numberOfRows] = - row._row[i]._coefficient / ci;
+        if ( row._row[i]._var >= _numberOfVariables - _numberOfRows && !FloatUtils::isZero( row._row[i]._coefficient ) )
+            coefficients[row._row[i]._var - _numberOfVariables + _numberOfRows] = row._row[i]._coefficient / ci;
     }
 
-    // If the lhs was part of original basis, its coefficient is 1 / ci
-    if (row._lhs >= _numberOfVariables - _numberOfRows )
-        coefficients[row._lhs - _numberOfVariables + _numberOfRows] = 1 / ci;
+    // If the lhs was part of original basis, its coefficient is -1 / ci
+    if ( row._lhs >= _numberOfVariables - _numberOfRows )
+        coefficients[row._lhs - _numberOfVariables + _numberOfRows] = -1 / ci;
 }
-
 
 void BoundExplainer::extractSparseRowCoefficients( const SparseUnsortedList &row, Vector<double> &coefficients, double ci ) const
 {
-    ASSERT(coefficients.size() == _numberOfRows );
+    ASSERT( coefficients.size() == _numberOfRows );
+    ASSERT( !FloatUtils::isZero( ci ) );
 
     // The coefficients of the row m highest-indices vars are the coefficients of slack variables
     for ( const auto &entry : row )
     {
-        if (entry._index >= _numberOfVariables - _numberOfRows && !FloatUtils::isZero( entry._value ) )
-            coefficients[entry._index - _numberOfVariables + _numberOfRows] = - entry._value / ci;
+        if ( entry._index >= _numberOfVariables - _numberOfRows && !FloatUtils::isZero( entry._value ) )
+            coefficients[entry._index - _numberOfVariables + _numberOfRows] = entry._value / ci;
     }
 }
 
@@ -213,7 +214,7 @@ void BoundExplainer::addVariable()
     _upperBoundExplanations.append( Vector<double>( 0 ) );
     _lowerBoundExplanations.append( Vector<double>( 0 ) );
 
-    for (unsigned i = 0; i < _numberOfVariables; ++i )
+    for ( unsigned i = 0; i < _numberOfVariables; ++i )
     {
         if ( !_upperBoundExplanations[i].empty() )
             _upperBoundExplanations[i].append( 0 );
@@ -225,13 +226,13 @@ void BoundExplainer::addVariable()
 
 void BoundExplainer::resetExplanation( unsigned var, bool isUpper )
 {
-    ASSERT(var < _numberOfVariables );
+    ASSERT( var < _numberOfVariables );
     isUpper ? _upperBoundExplanations[var].clear() : _lowerBoundExplanations[var].clear();
 }
 
-void BoundExplainer::injectExplanation( const Vector<double> &explanation, unsigned var, bool isUpper )
+void BoundExplainer::setExplanation( const Vector<double> &explanation, unsigned var, bool isUpper )
 {
-    ASSERT(var < _numberOfVariables && (explanation.empty() || explanation.size() == _numberOfRows ) );
+    ASSERT( var < _numberOfVariables && (explanation.empty() || explanation.size() == _numberOfRows ) );
     Vector<double> *temp = isUpper ? &_upperBoundExplanations[var] : &_lowerBoundExplanations[var];
     *temp = explanation;
 }
