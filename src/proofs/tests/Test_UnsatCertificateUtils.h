ccf10776b3790e24252f69d230e0fe9cb2923f18
OmriIsacHUJI <99890010+OmriIsacHUJI@users.noreply.github.com>
2022-03-15 17:30:05 +0200

    Update Test_UnsatCertificateUtils.h

--- src/proofs/tests/Test_UnsatCertificateUtils.h
+++ src/proofs/tests/Test_UnsatCertificateUtils.h
@@ -29,25 +29,26 @@ public:
 
         Vector<double> groundUpperBounds = { 1, 1 ,1 ,1 ,1 ,1 };
         Vector<double> groundLowerBounds = { 0, 0, 0, 0, 0, 0 };
-
-        Vector<double> explanation = { 1, 1, 0 };
-        // Linear combination is x0 = x1 - x2 - x3 - x4, thus explanation combination is only lhs
         Vector<double> rowCombination;
+
+        double explanation[3] = { 1, 1, 0 };
+        // Linear combination is x0 = 2x0 - x1 + x2 + x3 + x4, thus explanation combination is only lhs
+        // Checks computation method only, since no actual bound will be explained this way
 
         unsigned var = 0;
 
         UNSATCertificateUtils::getExplanationRowCombination( var, rowCombination, explanation, initialTableau );
 
         auto it = rowCombination.begin();
-        TS_ASSERT_EQUALS( *it, 0 );
+        TS_ASSERT_EQUALS( *it, 2 );
+        ++it;
+        TS_ASSERT_EQUALS( *it, -1 );
         ++it;
         TS_ASSERT_EQUALS( *it, 1 );
         ++it;
-        TS_ASSERT_EQUALS( *it, -1 );
+        TS_ASSERT_EQUALS( *it, 1 );
         ++it;
-        TS_ASSERT_EQUALS( *it, -1 );
-        ++it;
-        TS_ASSERT_EQUALS( *it, -1 );
+        TS_ASSERT_EQUALS( *it, 1 );
         ++it;
         TS_ASSERT_EQUALS( *it, 0 );
         ++it;
@@ -55,6 +56,6 @@ public:
 
         double explainedBound = UNSATCertificateUtils::computeBound( var, true, explanation, initialTableau, groundUpperBounds, groundLowerBounds );
 
-        TS_ASSERT_EQUALS( explainedBound, 1 );
+        TS_ASSERT_EQUALS( explainedBound, 5 );
     }
 };
