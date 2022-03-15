51506e54c537927d2f7c86df2bd54c44b417bdb0
OmriIsacHUJI <99890010+OmriIsacHUJI@users.noreply.github.com>
2022-03-15 17:57:54 +0200

    Update UnsatCertificateUtils.h
    When checking an explanation, it is received as an array (rather than a vector).
    Changed the row produced from an explanation, to match the changes in BoundExplainer.

--- src/proofs/UnsatCertificateUtils.h
+++ src/proofs/UnsatCertificateUtils.h
@@ -27,13 +27,19 @@ public:
       Use explanation to compute a bound (aka explained bound)
       Given a variable, an explanation, initial tableau and ground bounds.
     */
-    static double computeBound( unsigned var, bool isUpper, const Vector<double> &explanation,
-                                const Vector<Vector<double>> &initialTableau, const Vector<double> &groundUpperBounds, const Vector<double> &groundLowerBounds );
+    static double computeBound( unsigned var,
+                                bool isUpper,
+                                const double *explanation,
+                                const Vector<Vector<double>> &initialTableau,
+                                const Vector<double> &groundUpperBounds,
+                                const Vector<double> &groundLowerBounds );
 
     /*
       Given a var, a tableau and a column vector, create a linear combination used to explain a bound
     */
-    static void getExplanationRowCombination( unsigned var, Vector<double> &explanationRowCombination, const Vector<double> &explanation,
+    static void getExplanationRowCombination( unsigned var,
+                                              Vector<double> &explanationRowCombination,
+                                              const double *explanation,
                                               const Vector<Vector<double>> &initialTableau );
 };
 
