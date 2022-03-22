6c99d6ebea70f162e3ebcec12682414ccc65f9ed
Haoze(Andrew) Wu <haozewu@stanford.edu>
2022-03-22 10:47:09 -0700

    Update MarabouNetworkONNX.py

--- maraboupy/MarabouNetworkONNX.py
+++ maraboupy/MarabouNetworkONNX.py
@@ -454,7 +454,7 @@ class MarabouNetworkONNX(MarabouNetwork.MarabouNetwork):
         # Get variables
         inputVars = self.varMap[inputName].reshape(numChannels, -1)
         outputVars = self.makeNewVariables(nodeName).reshape(numChannels, -1)
-        assert(inputVars,shape == outputVars.shape)
+        assert(inputVars.shape == outputVars.shape)
 
         numInputs = inputVars.shape[1]
 
