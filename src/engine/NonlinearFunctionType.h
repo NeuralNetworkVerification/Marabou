7571d0e9175a244d71e6988a262b2bad5f359494
anwu1219 <haozewu@stanford.edu>
2022-06-17 08:10:09 -0700

    nonlinear

---
+++ src/engine/NonlinearFunctionType.h
@@ -0,0 +1,32 @@
+/*********************                                                        */
+/*! \file NonlinearFunctionType.h
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Teruhiro Tagomori
+ ** This file is part of the Marabou project.
+ ** Copyright (c) 2017-2019 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved. See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** [[ Add lengthier description here ]]
+
+ **/
+
+#ifndef __NonlinearFunctionType_h__
+#define __NonlinearFunctionType_h__
+
+enum NonlinearFunctionType {
+    SIGMOID = 0,
+    SOFTMAX = 1,
+};
+
+#endif // __NonlinearFunctionType_h__
+
+//
+// Local Variables:
+// compile-command: "make -C ../.. "
+// tags-file-name: "../../TAGS"
+// c-basic-offset: 4
+// End:
+//
