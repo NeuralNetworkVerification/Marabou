7362cd73e302fc2fae74ccb9a0bbdb8006f01dc5
Aleksandar Zeljic <zeljic@stanford.edu>
2022-11-18 10:40:58 -0800

    Add cvc5 include directory

---
+++ src/include/cvc5_private.h
@@ -0,0 +1,22 @@
+/******************************************************************************
+ * Top contributors (to current version):
+ *   Aina Niemetz, Mathias Preiner
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2022 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Inclusion of this file marks a header as private and generates a warning
+ * when the file is included improperly.
+ */
+
+#ifndef CVC5_PRIVATE_H
+#define CVC5_PRIVATE_H
+
+#include "cvc5_public.h"
+
+#endif /* CVC5_PRIVATE_H */
