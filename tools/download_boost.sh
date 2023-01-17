f04d754b24a67020d9086b1befc67818589ad149
anwu1219 <haozewu@stanford.edu>
2023-01-17 15:18:07 -0800

    update boost version, and CI modes

--- tools/download_boost.sh
+++ tools/download_boost.sh
@@ -7,11 +7,11 @@ cd $mydir
 # TODO: add progress bar, -q is quite, if removing it the progress bar is in
 # multiple lines
 echo "downloading boost"
-wget -q https://sourceforge.net/projects/boost/files/boost/1.68.0/boost_1_68_0.tar.gz/download -O boost_1_68_0.tar.gz
+wget -q https://sourceforge.net/projects/boost/files/boost/1.80.0/boost_1_80_0.tar.gz/download -O boost_1_80_0.tar.gz
 echo "unzipping boost"
-tar xzvf boost_1_68_0.tar.gz >> /dev/null
+tar xzvf boost_1_80_0.tar.gz >> /dev/null
 echo "installing boost"
-cd boost_1_68_0;
+cd boost_1_80_0;
 
 if [[ $OSTYPE == 'darwin'* ]]; then
     export SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
@@ -19,9 +19,9 @@ fi
 
 mkdir installed
 ./bootstrap.sh --prefix=`pwd`/installed --with-libraries=program_options,timer,chrono,thread >> /dev/null ;
-./b2 cxxflags=-fPIC link=static install >> /dev/null
+./b2 cxxflags=-fPIC link=static cxxflags=-std=c++11 install >> /dev/null
 mkdir installed32
 ./bootstrap.sh --prefix=`pwd`/installed32 --with-libraries=program_options,timer,chrono,thread >> /dev/null ;
-./b2 cxxflags=-fPIC link=static install address-model=32 >> /dev/null
+./b2 cxxflags=-fPIC link=static cxxflags=-std=c++11 install address-model=32 >> /dev/null
 
 cd $curdir
