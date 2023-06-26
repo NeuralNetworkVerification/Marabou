1d3bda823fb801b2db4930fca5e7b3d0594765e2
Teruhiro Tagomori <tagomaru@users.noreply.github.com>
2023-06-26 09:28:59 -0700

    Feature to calculate and retrieve output bounds (#636)
    * beofre change of MarabouCore
    
    * MarabouCore output interface is done
    
    * infeasible check
    
    * change docs and a comment
    
    * add test_calc_output_bounds
    
    * add test code
    
    * change comments and etc
    
    * fix typo
    
    * rollback redirect stuff
    
    * fix unclear type 'uint'
    
    * rollback to uint
    
    * remove unnecessary lines
    
    * remove initializeBoundsAndConstraintWatchersInTableau
    
    * remove unnecessary lines

--- maraboupy/test/test_network.py
+++ maraboupy/test/test_network.py
@@ -659,6 +659,48 @@ def test_local_robustness_sat_of_onnx_3D():
     # should be not local robustness
     assert(len(vals) > 0)
 
+def test_calculate_bounds():
+    """
+    Tests calculate bounds of an onnx network
+    """
+    filename = "fc_2-2-3.onnx"
+    options = Marabou.createOptions(verbosity = 0)
+
+    # Not UNSAT case
+    network = loadNetworkInONNX(filename)
+    inputVars = network.inputVars[0][0]
+    network.setLowerBound(inputVars[0], 3)
+    network.setUpperBound(inputVars[0], 4)
+    network.setLowerBound(inputVars[1], -2)
+    network.setUpperBound(inputVars[1], -1)
+
+    # calculate bounds
+    exitCode, vals, _ = network.calculateBounds(options = options)
+
+    # exitCode should be empty 
+    assert(exitCode == '')
+
+    # output bounds should be correct
+    assert(vals[network.outputVars[0][0][0]] == (2.0, 6.0))
+    assert(vals[network.outputVars[0][0][1]] == (-3.0, -1.0))
+    assert(vals[network.outputVars[0][0][2]] == (1.0, 3.0))
+
+    # UNSAT case
+    network = loadNetworkInONNX(filename)
+    inputVars = network.inputVars[0][0]
+    outputVars = network.outputVars[0]
+    network.setLowerBound(inputVars[0], 3)
+    network.setUpperBound(inputVars[0], 4)
+    network.setLowerBound(inputVars[1], -2)
+    network.setUpperBound(inputVars[1], -1)
+    network.setUpperBound(outputVars[0][0], 1)
+
+    # calculate bounds
+    exitCode, vals, _ = network.calculateBounds(options = options)
+
+    # exitCode should be unsat 
+    assert(exitCode == 'unsat')
+
 def loadNetwork(filename):
     # Load network relative to this file's location
     filename = os.path.join(os.path.dirname(__file__), NETWORK_FOLDER, filename)
