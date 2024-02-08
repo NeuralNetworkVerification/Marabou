958da7c1c234e6ca43de79a03330f41636fb8333
Haoze(Andrew) Wu <haozewu@stanford.edu>
2024-02-07 21:56:08 -0800

    remove redundant addend in equation (#716)
    * remove reundant addend
    
    * whoops
    
    * Update CHANGELOG.md
    
    * Update Equation.h
    
    * Update Equation.h

---
+++ src/engine/tests/Test_Equation.h
@@ -0,0 +1,54 @@
+/*********************                                                        */
+/*! \file Test_Equation.h
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Andrew Wu
+ ** This file is part of the Marabou project.
+ ** Copyright (c) 2017-2019 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved. See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** [[ Add lengthier description here ]]
+
+**/
+
+#include <cxxtest/TestSuite.h>
+
+#include "Equation.h"
+
+class EquationTestSuite : public CxxTest::TestSuite
+{
+public:
+
+    void test_remove_redundant_addends()
+    {
+        Equation eq;
+        eq.addAddend(1.5, 1);
+        eq.addAddend(1.5, 1);
+        eq.addAddend(-1, 2);
+        eq.addAddend(1, 2);
+        eq.addAddend(-0.5, 1);
+        eq.addAddend(0, 1);
+        eq.addAddend(0, 4);
+        eq.addAddend(3.5, 5);
+        eq.setScalar(1);
+
+        TS_ASSERT_EQUALS( eq._addends.size(), 8u );
+        TS_ASSERT( eq.containsRedundantAddends() );
+
+        // After transformation, should become:
+        // 2.5 x1 + 3.5 x5 = 1
+        TS_ASSERT_THROWS_NOTHING( eq.removeRedundantAddends() );
+        eq.dump();
+        TS_ASSERT_EQUALS( eq._addends.size(), 2u );
+        Set<unsigned> participatingVariables = eq.getParticipatingVariables();
+        TS_ASSERT_EQUALS( participatingVariables.size(), 2u );
+        TS_ASSERT( participatingVariables.exists( 1 ) );
+        TS_ASSERT( participatingVariables.exists( 5 ) );
+        TS_ASSERT_EQUALS( eq.getCoefficient( 1 ), 2.5 );
+        TS_ASSERT_EQUALS( eq.getCoefficient( 5 ), 3.5 );
+        TS_ASSERT_EQUALS( eq.getCoefficient( 0 ), 0 );
+        TS_ASSERT( !eq.containsRedundantAddends() );
+    }
+};
