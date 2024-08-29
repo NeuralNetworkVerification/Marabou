def3a1cbfcaba31c372f6bee902d04dc973238b1
mayaswissa <mayaswissa@gmail.com>
2024-08-29 16:05:33 +0300

    change default restarts

--- src/engine/PGD.h
+++ src/engine/PGD.h
@@ -7,7 +7,7 @@
 
 constexpr float LR = 0.05;
 constexpr unsigned DEFAULT_NUM_ITER = 100;
-constexpr unsigned DEFAULT_NUM_RESTARTS = 4;
+constexpr unsigned DEFAULT_NUM_RESTARTS = 3;
 constexpr unsigned INPUT = 0;
 constexpr unsigned OUTPUT = 1;
 constexpr unsigned RANGE = 1000.0f;
