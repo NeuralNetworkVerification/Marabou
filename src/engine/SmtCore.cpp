2ade45d40812fdde1bdbc52ef21c27a9fac99d35
idan0610 <idan0610@gmail.com>
2024-09-01 11:58:29 +0300

    VSIDS V5 - prefer literals that negate clauses, instead of satisfying them

--- src/engine/SmtCore.cpp
+++ src/engine/SmtCore.cpp
@@ -830,7 +830,7 @@ int SmtCore::cb_decide()
             continue;
 
         // For stability
-        if ( _cadicalVarToPlc[abs( literal )]->getLiteralForDecision() != literal )
+        if ( _cadicalVarToPlc[abs( literal )]->getLiteralForDecision() == literal )
             continue;
 
         unsigned numOfClausesSatisfiedByLiteral = 0;
@@ -838,7 +838,7 @@ int SmtCore::cb_decide()
             if ( !isClauseSatisfied( clause ) )
                 ++numOfClausesSatisfiedByLiteral;
 
-        scores[literal] = numOfClausesSatisfiedByLiteral;
+        scores[-literal] = numOfClausesSatisfiedByLiteral;
     }
 
     for (PiecewiseLinearConstraint *plc : _constraintToViolationCount.keys())
